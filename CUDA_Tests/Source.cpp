1206f6b399293100908f0b3ef25221c38af883c9
theProgrammingBox <justinboxgea@gmail.com>
2023-01-03 23:46:46 -0800

    cleaner rand

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -10,25 +10,24 @@ using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::nanoseconds;
 
-struct xorwow32	//curand
+struct xorwow32
 {
 	uint32_t state[6];
+	
+	xorwow32(uint32_t seed) : state{ 
+		seed ^ 123456789,
+		seed ^ 362436069,
+		seed ^ 521288629,
+		seed ^ 88675123,
+		seed ^ 5783321,
+		seed ^ 6615241 } {}
+	
 	uint32_t operator()()
 	{
 		uint32_t t = state[0] ^ (state[0] >> 2);
-		memcpy(state, state + 1, 4 * sizeof(uint32_t));
+		memcpy(state, state + 1, 16);
 		state[4] ^= (state[4] << 4) ^ (t ^ (t << 1));
 		return (state[5] += 362437) + state[4];
-	}
-	
-	xorwow32(uint32_t seed)
-	{
-		state[0] = seed ^ 123456789;
-		state[1] = seed ^ 362436069;
-		state[2] = seed ^ 521288629;
-		state[3] = seed ^ 88675123;
-		state[4] = seed ^ 5783321;
-		state[5] = seed ^ 6615241;
 	}
 };
 
