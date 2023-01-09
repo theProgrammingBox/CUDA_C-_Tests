fb8fff3c93cc75be82db8d719dbfab58cbaca93e
theProgrammingBox <justinboxgea@gmail.com>
2023-01-08 20:21:02 -0800

    I am still working out the personalities

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -41,60 +41,10 @@ static struct xorwow32
 	}
 } random(duration_cast<nanoseconds>(high_resolution_clock::now().time_since_epoch()).count());
 
-const static void cpuSgemmStridedBatched(
-	bool transB, bool transA,
-	int CCols, int CRows, int AColsBRows,
-	const float* alpha,
-	float* B, int ColsB, int SizeB,
-	float* A, int ColsA, int SizeA,
-	const float* beta,
-	float* C, int ColsC, int SizeC,
-	int batchCount)
-{
-	for (int b = batchCount; b--;)
-	{
-		for (int m = CCols; m--;)
-			for (int n = CRows; n--;)
-			{
-				float sum = 0;
-				for (int k = AColsBRows; k--;)
-					sum += (transA ? A[k * ColsA + n] : A[n * ColsA + k]) * (transB ? B[m * ColsB + k] : B[k * ColsB + m]);
-				C[n * ColsC + m] = *alpha * sum + *beta * C[n * ColsC + m];
-			}
-		A += SizeA;
-		B += SizeB;
-		C += SizeC;
-	}
-}
-
 const static void cpuGenerateUniform(float* matrix, uint32_t size, float min, float max)
 {
 	for (uint32_t counter = size; counter--;)
 		matrix[counter] = random(min, max);
-}
-
-const static void cpuClippedLinearUnit(float* inputMatrix, float* outputMatrix, uint32_t size)
-{
-	float input;
-	for (size_t counter = size; counter--;)
-	{
-		input = inputMatrix[counter] + 1;
-		input = (input > 0) * input - 2;
-		outputMatrix[counter] = (input < 0) * input + 1;
-	}
-}
-
-const static void cpuClippedLinearUnitGradient(float* inputMatrix, float* gradientMatrix, float* outputMatrix, uint32_t size) {
-	float input;
-	float gradient;
-	bool greaterOne;
-	for (size_t counter = size; counter--;)
-	{
-		input = inputMatrix[counter];
-		gradient = gradientMatrix[counter];
-		bool greaterZero = (uint32_t)gradient & 0x80000000;
-		outputMatrix[counter] = (greaterZero ^ (input > 1) || greaterZero ^ (input > -1)) * gradient;
-	}
 }
 
 const static void cpuSoftmax(float* inputMatrix, float* outputMatrix, uint32_t size)
@@ -122,70 +72,129 @@ const static void cpuSoftmaxGradient(float* outputMatrix, float* gradient, uint3
 }
 
 int main() {
-	constexpr uint32_t N = 3;
-	constexpr uint32_t ITERATIONS = 1000;
+	constexpr uint32_t N = 2;
+	constexpr uint32_t ITERATIONS = 100000;
 	constexpr float LEARNING_RATE = 0.1f;
+
+	// Prisoner's Dilemma, score is time in prison
+	float score[N * N] = {
+		5, 10,	// (Snitch1 & Snitch2) | (Silent1 & Snitch2)
+		0, 1	// (Snitch1 & Silent2) | (Silent1 & Silent2)
+	};
+
+	enum Personality {
+		MALICIOUS = 0,		// always tries to force the other player's time in prison to be greater than their own
+		LOVER = 1,			// always tries to force the other player's time in prison to be less than their own
+		COOPERATIVE = 2,	// always tries to minimize their own time in prison
+		SUISIDAL = 3,		// always tries to maximize their own time in prison
+	};
 	
-	float goal[N] = {};	// target probabilities
-	float bias[N] = {};	// bias that is converted to probabilities
-	float res[N] = {};	// resulting probabilities after softmax bias
-	float grad[N] = {};	// gradient of the loss function
-	uint32_t sample = 0;// sampled index from the probability distribution
-	float gradient;		// whether the sampled index should be increased or decreased
-
-	cpuGenerateUniform(bias, N, -1, 1);
-	cpuSoftmax(bias, goal, N);	// generate random target probabilities
-
-	cpuGenerateUniform(bias, N, -1, 1);	// generate random bias
-
-	int iter = ITERATIONS;
-	while (iter--)
+	uint32_t sample1 = 0;	// player 1's sampled index from the probability distribution
+	uint32_t sample2 = 0;	// player 2's sampled index from the probability distribution
+	float probabilityGrad1;	// whether player 1's sampled action should be increased or decreased
+	float probabilityGrad2;	// whether player 2's sampled action should be increased or decreased
+	float randNum;			// random number used to sample from the probability distribution
+	float score1;			// player 1's score
+	float score2;			// player 2's score
+	
+	float bias[N];
+	float result[N];
+	float gradient1[N];
+	float gradient2[N];
+	
+	for (uint32_t personality = 4; personality--;)
 	{
-		// calculate probabilities from bias
-		cpuSoftmax(bias, res, N);
-		
-		// Sample from the distribution
-		float randNum = random(0, 1);
-		for (int i = 0; i < N; i++)
+		// Initialize biases so the probability distribution is uniform
+		memset(bias, 0, N * sizeof(float));
+
+		int iter = ITERATIONS;
+		while (iter--)
 		{
-			randNum -= res[i];
-			if (randNum <= 0)
+			// calculate probabilities from bias
+			cpuSoftmax(bias, result, N);
+
+			// Sample from the distribution
+			randNum = random(0, 1);
+			for (int i = 0; i < N; i++)
 			{
-				sample = i;
+				randNum -= result[i];
+				if (randNum <= 0)
+				{
+					sample1 = i;
+					break;
+				}
+			}
+
+			randNum = random(0, 1);
+			for (int i = 0; i < N; i++)
+			{
+				randNum -= result[i];
+				if (randNum <= 0)
+				{
+					sample2 = i;
+					break;
+				}
+			}
+
+			// Calculate the score
+			score1 = score[sample2 * N + sample1];
+			score2 = score[sample1 * N + sample2];
+
+			// should the chances of the sampled move be increased or decreased?
+
+			switch (personality)
+			{
+			case MALICIOUS:
+				probabilityGrad1 = (score2 > score1) * LEARNING_RATE;
+				probabilityGrad2 = (score1 > score2) * LEARNING_RATE;
+				break;
+			case LOVER:
+				probabilityGrad1 = (score2 < score1) * LEARNING_RATE;
+				probabilityGrad2 = (score1 < score2) * LEARNING_RATE;
+				break;
+			case COOPERATIVE:
+				probabilityGrad1 = (score1 > 0) * LEARNING_RATE;
+				probabilityGrad2 = (score2 > 0) * LEARNING_RATE;
+				break;
+			case SUISIDAL:
+				probabilityGrad1 = (score1 < 10) * LEARNING_RATE;
+				probabilityGrad2 = (score2 < 10) * LEARNING_RATE;
 				break;
 			}
+
+			// calculate gradient of the loss function
+			cpuSoftmaxGradient(result, &probabilityGrad1, &sample1, gradient1, N);
+			cpuSoftmaxGradient(result, &probabilityGrad2, &sample2, gradient2, N);
+
+			// update bias
+			for (int i = 0; i < N; i++)
+				bias[i] += gradient1[i] + gradient2[i];
 		}
-		
-		// should the sampled index be increased or decreased
-		gradient = res[sample] > goal[sample] ? -LEARNING_RATE : LEARNING_RATE;
-		
-		// calculate gradient of the loss function
-		cpuSoftmaxGradient(res, &gradient, &sample, grad, N);
-		
-		// update bias
+
+		cout << "Result:\n";
+		cout << "If both personalities are ";
+		switch (personality)
+		{
+		case MALICIOUS:
+			cout << "MALICIOUS";
+			break;
+		case LOVER:
+			cout << "LOVER";
+			break;
+		case COOPERATIVE:
+			cout << "COOPERATIVE";
+			break;
+		case SUISIDAL:
+			cout << "SUISIDAL";
+			break;
+		}
+		cout << ", the optimal strategy is to Snitch " << result[0] * 100 << "% of the time and Silent " << result[1] * 100 << "% of the time.\n\n";
+
+		/*cout << "Bias: ";
 		for (int i = 0; i < N; i++)
-			bias[i] += grad[i];
+			cout << bias[i] << " ";
+		cout << "\n";*/
 	}
-	
-	cout << "Goal: ";
-	for (int i = 0; i < N; i++)
-		cout << goal[i] << " ";
-	cout << "\n";
-
-	cout << "Res: ";
-	for (int i = 0; i < N; i++)
-		cout << res[i] << " ";
-	cout << "\n";
-
-	cout << "Error: ";
-	for (int i = 0; i < N; i++)
-		cout << abs(goal[i] - res[i]) << " ";
-	cout << "\n";
-
-	cout << "Bias: ";
-	for (int i = 0; i < N; i++)
-		cout << bias[i] << " ";
-	cout << "\n";
 
 	return 0;
 }
