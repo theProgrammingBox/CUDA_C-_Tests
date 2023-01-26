5afa24b32718432b48e401162d10ec4591a6de47
theProgrammingBox <justinboxgea@gmail.com>
2023-01-25 17:44:32 -0800

    working with vectors of dynamic arrays

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <math.h>
 #include <fstream>
+#include <memory>
 
 using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
@@ -17,6 +18,8 @@ using std::sort;
 using std::ceil;
 using std::exp;
 using std::ofstream;
+using std::unique_ptr;
+using std::make_unique;
 
 class Random
 {
@@ -96,76 +99,38 @@ private:
 namespace GlobalVars
 {
 	Random random(Random::MakeSeed(0));
+	constexpr uint32_t ACTIONS = 3;
+	constexpr uint32_t NUM = 5;
 }
 
-class Player
+struct MyStruct
 {
-public:
-	uint32_t move()
-	{
-		return GlobalVars::random.Ruint32() % 9;
-	}
-};
-
-class TicTacToe
-{
-public:
-	bool player;
-	float board[9];
-	Player players[2];
-	uint32_t runningState;
-	uint32_t moveCount;
-
-	void Run()
-	{
-		player = GlobalVars::random.Ruint32() & 1;
-		memset(board, 0, sizeof(board));
-		runningState = 0;
-		moveCount = 0;
-
-		while (!runningState)
-		{
-			player = !player;
-			uint32_t action = players[player].move();
-			if (!board[action])
-			{
-				board[action] = (player << 1) - 1.0f;
-				moveCount++;
-
-				for (uint32_t i = 0; i < 3; i++)
-					runningState |= board[i] && board[i] == board[i + 3] && board[i] == board[i + 6];
-				for (uint32_t i = 0; i < 9; i += 3)
-					runningState |= board[i] && board[i] == board[i + 1] && board[i] == board[i + 2];
-				runningState |= board[0] && board[0] == board[4] && board[0] == board[8];
-				runningState |= board[2] && board[2] == board[4] && board[2] == board[6];
-				runningState |= (moveCount == 9) << 1;
-			}
-			else
-				runningState |= 4;
-		}
-
-		if (runningState & 4)
-			cout << "Winner by Invalid move: " << (!player << 1) - 1.0f << "\n";
-		else if (runningState & 1)
-			cout << "Winner: " << (player << 1) - 1.0f << "\n";
-		else if (runningState & 2)
-			cout << "Draw\n";
-
-		for (uint32_t i = 0; i < 3; i++)
-		{
-			for (uint32_t j = 0; j < 3; j++)
-			{
-				cout << board[i * 3 + j] << " ";
-			}
-			cout << "\n";
-		}
-	}
+	int x;
+	int* dynamicArray;
+	int arraySize;
+	
+	MyStruct(int size) : x(0), arraySize(size), dynamicArray(new int[size]) {}
+	MyStruct(const MyStruct& other) : x(other.x), arraySize(other.arraySize), dynamicArray(new int[other.arraySize])
+	{ std::copy(other.dynamicArray, other.dynamicArray + arraySize, dynamicArray); }
+	~MyStruct() { delete[] dynamicArray; }
 };
 
 int main()
 {
-	TicTacToe game;
-	game.Run();
+	vector<MyStruct> myVector;
+	
+	int size = 5;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = i;
+	}
+
+	MyStruct newStruct(size);
+	newStruct.x = 10;
+	newStruct.dynamicArray = arr;
+	newStruct.arraySize = size;
+
+	myVector.push_back(newStruct);
 
 	return 0;
 }
