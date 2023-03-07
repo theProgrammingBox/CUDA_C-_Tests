a90200ffe44ac2f772802606876695e49ee19523
theProgrammingBox <justinboxgea@gmail.com>
2023-03-07 15:42:37 -0800

    testing vector storage

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -6,28 +6,35 @@ class Test
 public:
 	struct TestStructParam
 	{
-		float* matrix;
+		float matrix[10];
 
 		TestStructParam()
 		{
-			matrix = new float[10];
+			for (int i = 0; i < 10; i++)
+				matrix[i] = i;
 		}
 	};
 	
 	struct TestStructCompute
 	{
 		TestStructParam* param;
-		float* matrix;
+		float matrix[10];
 
-		TestStructCompute(TestStructParam* param)
+		TestStructCompute()
+		{
+		}
+
+		void Compute(TestStructParam* param, float num, uint32_t idx)
 		{
 			this->param = param;
-			matrix = new float[10];
+			for (int i = 0; i < 10; i++)
+				matrix[i] = param->matrix[i];
+			matrix[idx] = num;
 		}
 	};
 
 	TestStructParam param;
-	std::vector<TestStructCompute> computes;
+	std::vector<TestStructCompute*> computes;
 
 	Test()
 	{
@@ -35,16 +42,21 @@ public:
 
 	~Test()
 	{
-		delete[] param.matrix;
-		for (auto& compute : computes)
-		{
-			delete[] compute.matrix;
-		}
 	}
 	
 	void AddCompute()
 	{
-		computes.emplace_back(TestStructCompute(&param));
+		TestStructCompute compute;
+		computes.emplace_back(&compute);
+		//computes.back().Compute(&param, 10.0f, 0);
+		compute.Compute(&param, 10.0f, 0);
+
+		for (auto& compute : computes)
+		{
+			for (int i = 0; i < 10; i++)
+				printf("%f ", compute->matrix[i]);
+			printf("\n");
+		}
 	}
 };
 
