46789e87dd4627cb9677542d97ff4395292179a7
theProgrammingBox <justinboxgea@gmail.com>
2023-03-07 15:44:50 -0800

    making it as similar to what I am implementing

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -24,12 +24,14 @@ public:
 		{
 		}
 
-		void Compute(TestStructParam* param, float num, uint32_t idx)
+		uint32_t Compute(TestStructParam* param, float num, uint32_t idx)
 		{
 			this->param = param;
 			for (int i = 0; i < 10; i++)
 				matrix[i] = param->matrix[i];
 			matrix[idx] = num;
+
+			return matrix[idx];
 		}
 	};
 
@@ -44,13 +46,15 @@ public:
 	{
 	}
 	
-	void AddCompute()
+	uint32_t AddCompute(float num, uint32_t idx)
 	{
 		TestStructCompute compute;
 		computes.emplace_back(&compute);
-		//computes.back().Compute(&param, 10.0f, 0);
-		compute.Compute(&param, 10.0f, 0);
+		return compute.Compute(&param, num, idx);
+	}
 
+	void Print()
+	{
 		for (auto& compute : computes)
 		{
 			for (int i = 0; i < 10; i++)
@@ -63,7 +67,8 @@ public:
 int main()
 {
 	Test test;
-	test.AddCompute();
+	printf("%d", test.AddCompute(50.0f, 5));
+	test.Print();
 	
 	return 0;
 }
