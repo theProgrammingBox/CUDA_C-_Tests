1383c1e56bc37687e4abff5c9e3b045229883a8e
theProgrammingBox <justinboxgea@gmail.com>
2023-03-14 13:56:11 -0700

    working on float

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -7,31 +7,26 @@ uint32_t LowLevelI32Add(uint32_t a, uint32_t b)
 	while (b)
 	{
 		carry = a & b;
-		a = a ^ b;
+		a ^= b;
 		b = carry << 1;
 	}
 	return a;
 }
 
-uint32_t LowLevelI32Abs(uint32_t a)
+uint32_t LowLevelI32Neg(uint32_t a)
 {
 	return LowLevelI32Add(~a, 1);
 }
 
-uint32_t LowLevelI32Mul(uint32_t a, uint32_t b)
+uint32_t LowLevelI32Mul(uint32_t x, uint32_t y)
 {
-	bool aPos = a & 0x80000000;
-	bool bPos = b & 0x80000000;
-	if (aPos) a = LowLevelI32Abs(a);
-	if (bPos) b = LowLevelI32Abs(b);
 	uint32_t result = 0;
-	while (b > 0)
+	while (y)
 	{
-		if (b & 1) result = LowLevelI32Add(result, a);
-		b >>= 1;
-		a <<= 1;
+		if (y & 1) result = LowLevelI32Add(result, x);
+		y >>= 1;
+		x <<= 1;
 	}
-	if (aPos ^ bPos) result = LowLevelI32Abs(result);
 	return result;
 }
 
@@ -41,29 +36,39 @@ uint32_t LowLevelI32Mul(uint32_t a, uint32_t b)
 	uint32_t bI = *(uint32_t*)&b;
 	bool signA = aI & 0x80000000;
 	bool signB = bI & 0x80000000;
+	uint32_t expA = (aI >> 23) & 0xFF;
+	uint32_t expB = (bI >> 23) & 0xFF;
+	uint32_t mantA = aI & 0x7FFFFF;
+	uint32_t mantB = bI & 0x7FFFFF;
+
 	
-	
-	return *(float*)&resultI;
+	return *(float*)& resultI;
 }*/
 
 uint32_t main()
 {
 	Random random(Random::MakeSeed(275));
-	for (uint32_t i = 10; i--;)
+	for (uint32_t itr = 10; itr--;)
 	{
 		int32_t a = (int32_t)random.Ruint32() * 0.0000001f;
 		int32_t b = (int32_t)random.Ruint32() * 0.0000001f;
 		int32_t c = a * b;
 		int32_t d = LowLevelI32Mul(a, b);
+		if (c != d)
+		printf("%d * %d = %d\n%d * %d = %d\n\n", a, b, c, a, b, d);
 
 		int32_t e = (int32_t)random.Ruint32() * 0.0000001f;
 		int32_t f = (int32_t)random.Ruint32() * 0.0000001f;
 		int32_t g = e + f;
 		int32_t h = LowLevelI32Add(e, f);
-		if (c ^ d)
-			printf("%d * %d = %d\n%d * %d = %d\n\n", a, b, c, a, b, d);
-		if (e ^ f)
-			printf("%d + %d = %d\n%d + %d = %d\n\n", e, f, g, e, f, h);
+		if (g != h)
+		printf("%d + %d = %d\n%d + %d = %d\n\n", e, f, g, e, f, h);
+
+		int32_t i = (int32_t)random.Ruint32() * 0.0000001f;
+		int32_t j = i * -1;
+		int32_t k = LowLevelI32Neg(i);
+		if (j != k)
+		printf("%d * -1 = %d\n%d * -1 = %d\n\n", i, j, i, k);
 	}
 	return 0;
 }
