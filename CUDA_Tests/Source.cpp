b9e4745d2e16ada082f033b13c8583f673cb18e5
theProgrammingBox <justinboxgea@gmail.com>
2023-03-14 13:18:21 -0700

    testing mul

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Random.h"
 
 int32_t LowLevelI32Add(int32_t a, int32_t b)
 {
@@ -12,12 +13,17 @@ int32_t LowLevelI32Add(int32_t a, int32_t b)
 	return a;
 }
 
+int32_t LowLevelI32Abs(int32_t a)
+{
+	return LowLevelI32Add(~a, 1);
+}
+
 int32_t LowLevelI32Mul(int32_t a, int32_t b)
 {
 	bool aPos = a & 0x80000000;
 	bool bPos = b & 0x80000000;
-	if (aPos) a = LowLevelI32Add(~a, 1);
-	if (bPos) b = LowLevelI32Add(~b, 1);
+	if (aPos) a = LowLevelI32Abs(a);
+	if (bPos) b = LowLevelI32Abs(b);
 	int32_t result = 0;
 	while (b > 0)
 	{
@@ -27,15 +33,25 @@ int32_t LowLevelI32Mul(int32_t a, int32_t b)
 		a <<= 1;
 	}
 	if (aPos ^ bPos)
-		result = LowLevelI32Add(~result, 1);
+		result = LowLevelI32Abs(result);
 	return result;
 }
 
 int32_t main()
 {
-	printf("LowLevelI32Add(5, 7) = %d\n", LowLevelI32Add(5, 7));
-	printf("LowLevelI32Add(5, -7) = %d\n", LowLevelI32Add(5, -7));
-	printf("multiply_bitewise(5, 7) = %d\n", LowLevelI32Mul(5, 7));
-	printf("multiply_bitewise(5, -7) = %d\n", LowLevelI32Mul(5, -7));
+	Random random(Random::MakeSeed());
+	for (uint32_t i = 1000; i--;)
+	{
+		int32_t a = random.Ruint32();
+		int32_t b = random.Ruint32();
+		int32_t c = a * b;
+		int32_t d = LowLevelI32Mul(a, b);
+		int32_t e = a + b;
+		int32_t f = LowLevelI32Add(a, b);
+		if (c ^ d)
+			printf("%d * %d = %d, %d * %d = %d\n", a, b, c, a, b, d);
+		if (e ^ f)
+			printf("%d + %d = %d, %d + %d = %d\n", a, b, e, a, b, f);
+	}
 	return 0;
 }
