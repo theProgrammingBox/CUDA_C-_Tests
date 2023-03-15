fb8ec0b0c9549ad274e8cda33f3b9cb658af58e8
theProgrammingBox <justinboxgea@gmail.com>
2023-03-14 21:14:31 -0700

    head hurts

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -25,6 +25,11 @@ uint32_t LowLevelI32Mul(uint32_t x, uint32_t y)
 	return result;
 }
 
+uint32_t LowLevelI32Flip(uint32_t x)
+{
+	return LowLevelI32Add(~x, 1);
+}
+
 uint64_t LowLevelI64Add(uint64_t a, uint64_t b)
 {
 	uint64_t carry;
@@ -49,75 +54,123 @@ uint64_t LowLevelI64Mul(uint64_t x, uint64_t y)
 	return result;
 }
 
-float LowLevelf32Mul(float a, float b)
+float LowLevelf32Mul(float x, float y)
 {
-	uint32_t aI = *(uint32_t*)&a;
-	uint32_t bI = *(uint32_t*)&b;
+	uint32_t I32x = *(uint32_t*)&x;
+	uint32_t I32y = *(uint32_t*)&y;
 	
-	uint64_t mantA = aI & 0x7FFFFF | 0x800000;
-	uint64_t mantB = bI & 0x7FFFFF | 0x800000;
-
-	uint32_t expA = aI >> 23 & 0xFF;
-	uint32_t expB = bI >> 23 & 0xFF;
-
-	uint64_t resultMant = LowLevelI64Mul(mantA, mantB);
-	uint32_t resultExp = LowLevelI32Add(LowLevelI32Add(expA, expB), -127);
-	bool roundUp = resultMant & 0x400000;
-	resultMant >>= 23;
+	uint64_t resultMantissa = LowLevelI64Mul((uint64_t)I32x & 0x7FFFFF | 0x800000, (uint64_t)I32y & 0x7FFFFF | 0x800000);
+	uint32_t resultExponent = LowLevelI32Add(LowLevelI32Add(I32x >> 23 & 0xFF, I32y >> 23 & 0xFF), -127);
+	bool roundUp = resultMantissa & 0x400000;
+	resultMantissa >>= 23;
 	
-	while (resultMant & 0xffffffffff000000)
+	while (resultMantissa & 0xffffffffff000000)
 	{
-		roundUp = resultMant & 1;
-		resultMant >>= 1;
-		resultExp = LowLevelI32Add(resultExp, 1);
+		roundUp = resultMantissa & 1;
+		resultMantissa >>= 1;
+		resultExponent = LowLevelI32Add(resultExponent, 1);
 	}
-	resultMant = LowLevelI64Add(resultMant, roundUp);
+	resultMantissa = LowLevelI64Add(resultMantissa, roundUp);
 	
-	while (!(resultMant & 0x800000))
+	while (resultMantissa && !(resultMantissa & 0x800000))
 	{
-		resultMant <<= 1;
-		resultExp = LowLevelI32Add(resultExp, -1);
+		resultMantissa <<= 1;
+		resultExponent = LowLevelI32Add(resultExponent, -1);
 	}
 	
-	uint32_t result = (aI ^ bI) & 0x80000000 | resultExp << 23 | (resultMant & 0x7FFFFF);
-
-	float ans = a * b;
-	uint32_t answer = *(uint32_t*)&ans;
-	if (answer ^ result)
-	{
-		printf("roundUp: %d\n", roundUp);
-		for (int32_t i = 31; i >= 0; i--)
-		{
-			printf("%d", (answer >> i) & 1);
-			if (i == 31 || i == 23) printf(" ");
-		}
-		printf("\n");
-
-		for (int32_t i = 31; i >= 0; i--)
-		{
-			printf("%d", (result >> i) & 1);
-			if (i == 31 || i == 23) printf(" ");
-		}
-		printf("\n\n");
-	}
+	uint32_t result = (I32x ^ I32y) & 0x80000000 | resultExponent << 23 | (resultMantissa & 0x7FFFFF);
 	
 	return *(float*)&result;
 }
 
-float LowLevelf32Add(float a, float b)
+float LowLevelf32Add(float x, float y)
 {
-	return 0;
+	uint32_t I32a = *(uint32_t*)&x;
+	uint32_t I32b = *(uint32_t*)&y;
+	
+	uint32_t xExponent = I32a >> 23 & 0xFF;
+	uint32_t yExponent = I32b >> 23 & 0xFF;
+
+	uint32_t xMantissa = I32a & 0x7FFFFF | 0x800000;
+	uint32_t yMantissa = I32b & 0x7FFFFF | 0x800000;
+
+	if (I32a & 0x80000000)
+		xMantissa = LowLevelI32Flip(xMantissa);
+	if (I32b & 0x80000000)
+		yMantissa = LowLevelI32Flip(yMantissa);
+
+	if (y > x)	// change
+	{
+		std::swap(I32a, I32b);
+		std::swap(xExponent, yExponent);
+		std::swap(xMantissa, yMantissa);
+	}
+
+	for (;;)
+	{
+		if (xExponent <= yExponent) break;
+		xMantissa >>= 1;
+		xExponent = LowLevelI32Add(xExponent, 1);
+		if (xExponent <= yExponent) break;
+		yMantissa <<= 1;
+		yExponent = LowLevelI32Add(yExponent, -1);
+	}
+
+	uint32_t resultMantissa = LowLevelI32Add(xMantissa, yMantissa);
+	uint32_t resultExponent = xExponent;
+	uint32_t resultSign = 0;
+
+	if (resultMantissa & 0x80000000)
+	{
+		resultSign = 0x80000000;
+		LowLevelI32Flip(resultMantissa);
+	}
+	
+	while (resultMantissa & 0xff000000)
+	{
+		resultMantissa >>= 1;
+		resultExponent = LowLevelI32Add(resultExponent, 1);
+	}
+
+	while (resultMantissa && !(resultMantissa & 0x800000))
+	{
+		resultMantissa <<= 1;
+		resultExponent = LowLevelI32Add(resultExponent, -1);
+	}
+	
+	uint32_t result = resultSign | resultExponent << 23 | (resultMantissa & 0x7FFFFF);
+	float answer = x + y;
+	uint32_t U32a = *(uint32_t*)&answer;
+
+	for (int32_t i = 31; i >= 0; i--)
+	{
+		printf("%d", (U32a >> i) & 1);
+		if (i == 31 || i == 23) printf(" ");
+	}
+	printf("\n");
+
+	for (int32_t i = 31; i >= 0; i--)
+	{
+		printf("%d", (result >> i) & 1);
+		if (i == 31 || i == 23) printf(" ");
+	}
+	printf("\n");
+
+	return *(float*)&result;
 }
 
 uint32_t main()
 {
 	Random random(Random::MakeSeed(275));
-	for (uint32_t itr = 1000000; itr--;)
+	for (uint32_t i = 10; i--;)
 	{
 		float x = random.Rfloat(-100, 100);
 		float y = random.Rfloat(-100, 100);
-		LowLevelf32Mul(x, y);
-		if (itr % 10000 == 0) printf("%d\n", itr);
+		//printf("%f * %f = %f\n", x, y, LowLevelf32Mul(x, y));
+		printf("%f + %f = %f\n", x, y, LowLevelf32Add(x, y));
+		printf("%f + %f = %f\n\n", x, y, x + y);
+		/*int32_t x = (int32_t)random.Ruint32() * 0.00000001f;
+		printf("%d = %d\n", x, LowLevelI32Flip(x));*/
 	}
 	
 	return 0;
