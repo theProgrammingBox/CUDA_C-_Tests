ebba2ac609e73ed46419e3c2513b4cd1e343df1a
theProgrammingBox <justinboxgea@gmail.com>
2023-03-14 19:00:19 -0700

    very very very close, just small edge cases left

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -13,11 +13,6 @@ uint32_t LowLevelI32Add(uint32_t a, uint32_t b)
 	return a;
 }
 
-uint32_t LowLevelI32Neg(uint32_t a)
-{
-	return LowLevelI32Add(~a, 1);
-}
-
 uint32_t LowLevelI32Mul(uint32_t x, uint32_t y)
 {
 	uint32_t result = 0;
@@ -56,35 +51,31 @@ uint64_t LowLevelI64Mul(uint64_t x, uint64_t y)
 
 float LowLevelf32Mul(float a, float b)
 {
-	float ans = a * b;
-	uint32_t answer = *(uint32_t*)&ans;
-	for (int32_t i = 31; i >= 0; i--)
-	{
-		printf("%d", (answer >> i) & 1);
-		if (i == 31 || i == 23) printf(" ");
-	}
-	printf("\n");
-	
 	uint32_t aI = *(uint32_t*)&a;
 	uint32_t bI = *(uint32_t*)&b;
-
-	uint64_t mantA = (aI & 0x7FFFFF) | 0x800000;
-	uint64_t mantB = (bI & 0x7FFFFF) | 0x800000;
+	
+	uint64_t mantA = aI & 0x7FFFFF | 0x800000;
+	uint64_t mantB = bI & 0x7FFFFF | 0x800000;
 
 	uint32_t expA = aI >> 23 & 0xFF;
 	uint32_t expB = bI >> 23 & 0xFF;
 
-	uint64_t resultMant = LowLevelI64Mul(mantA, mantB) >> 23;
+	uint64_t resultMant = LowLevelI64Mul(mantA, mantB);
 	uint32_t resultExp = LowLevelI32Add(LowLevelI32Add(expA, expB), -127);
-
-	// the problem is rounding, when I shift to the right, I am removing data
+	bool lastBit = resultMant & 0x400000;
+	resultMant >>= 23;
 	
-	while (resultMant & 0xffffffffff800000)
-	{
-		resultMant >>= 1;
-		resultExp = LowLevelI32Add(resultExp, 1);
-	}
-
+	/*do
+	{*/
+		while (resultMant & 0xffffffffff000000)
+		{
+			lastBit = resultMant & 1;
+			resultMant >>= 1;
+			resultExp = LowLevelI32Add(resultExp, 1);
+		}
+		resultMant = LowLevelI64Add(resultMant, lastBit);
+	//} while (resultMant & 0xffffffffff000000);
+	
 	while (!(resultMant & 0x800000))
 	{
 		resultMant <<= 1;
@@ -93,49 +84,41 @@ float LowLevelf32Mul(float a, float b)
 	
 	uint32_t result = (aI ^ bI) & 0x80000000 | resultExp << 23 | (resultMant & 0x7FFFFF);
 
-	for (int32_t i = 31; i >= 0; i--)
+	float ans = a * b;
+	uint32_t answer = *(uint32_t*)&ans;
+	if (answer ^ result)
 	{
-		printf("%d", (result >> i) & 1);
-		if (i == 31 || i == 23) printf(" ");
+		for (int32_t i = 31; i >= 0; i--)
+		{
+			printf("%d", (answer >> i) & 1);
+			if (i == 31 || i == 23) printf(" ");
+		}
+		printf("\n");
+
+		for (int32_t i = 31; i >= 0; i--)
+		{
+			printf("%d", (result >> i) & 1);
+			if (i == 31 || i == 23) printf(" ");
+		}
+		printf("\n\n");
 	}
-	printf("\n");
 	
 	return *(float*)&result;
 }
 
 float LowLevelf32Add(float a, float b)
 {
-	
 	return 0;
 }
 
 uint32_t main()
 {
 	Random random(Random::MakeSeed(275));
-	for (uint32_t itr = 10; itr--;)
+	for (uint32_t itr = 100000000000; itr--;)
 	{
-		int32_t a = (int32_t)random.Ruint32() * 0.0000001f;
-		int32_t b = (int32_t)random.Ruint32() * 0.0000001f;
-		int32_t c = a * b;
-		int32_t d = LowLevelI32Mul(a, b);
-		if (c != d) printf("%d * %d = %d\n%d * %d = %d\n\n", a, b, c, a, b, d);
-
-		int32_t e = (int32_t)random.Ruint32() * 0.0000001f;
-		int32_t f = (int32_t)random.Ruint32() * 0.0000001f;
-		int32_t g = e + f;
-		int32_t h = LowLevelI32Add(e, f);
-		if (g != h) printf("%d + %d = %d\n%d + %d = %d\n\n", e, f, g, e, f, h);
-
-		int32_t i = (int32_t)random.Ruint32() * 0.0000001f;
-		int32_t j = i * -1;
-		int32_t k = LowLevelI32Neg(i);
-		if (j != k) printf("%d * -1 = %d\n%d * -1 = %d\n\n", i, j, i, k);
-
-		float l = random.Rfloat(-100, 100);
-		float m = random.Rfloat(-100, 100);
-		float n = l * m;
-		float o = LowLevelf32Mul(l, m);
-		if (n != o) printf("%f * %f = %f\n%f * %f = %f\n\n", l, m, n, l, m, o);
+		float x = random.Rfloat(-100, 100);
+		float y = random.Rfloat(-100, 100);
+		LowLevelf32Mul(x, y);
 	}
 	
 	return 0;
