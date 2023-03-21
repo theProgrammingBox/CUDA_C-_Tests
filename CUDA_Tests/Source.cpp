6c7ffb46a91bd05ae2d45369c4f610297e64fa4c
theProgrammingBox <justinboxgea@gmail.com>
2023-03-21 15:47:19 -0700

    weird, for saxplyEX, it still does f32 math of f16

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,181 +1,95 @@
+#include <cudnn.h>
+#include <cublas_v2.h>
+#include <curand.h>
+#include <cuda_fp16.h>
 #include <iostream>
-#include "Random.h"
 
-uint32_t LowLevelI32Add(uint32_t a, uint32_t b)
+void PrintMatrixF16(__half* arr, uint32_t rows, uint32_t cols, const char* label)
 {
-	uint32_t carry;
-	while (b)
+	printf("%s:\n", label);
+	for (uint32_t i = 0; i < rows; i++)
 	{
-		carry = a & b;
-		a ^= b;
-		b = carry << 1;
-	}
-	return a;
-}
-
-uint32_t LowLevelI32Mul(uint32_t x, uint32_t y)
-{
-	uint32_t result = 0;
-	while (y)
-	{
-		if (y & 1) result = LowLevelI32Add(result, x);
-		y >>= 1;
-		x <<= 1;
-	}
-	return result;
-}
-
-uint32_t LowLevelI32Flip(uint32_t x)
-{
-	return LowLevelI32Add(~x, 1);
-}
-
-uint64_t LowLevelI64Add(uint64_t a, uint64_t b)
-{
-	uint64_t carry;
-	while (b)
-	{
-		carry = a & b;
-		a ^= b;
-		b = carry << 1;
-	}
-	return a;
-}
-
-uint64_t LowLevelI64Mul(uint64_t x, uint64_t y)
-{
-	uint64_t result = 0;
-	while (y)
-	{
-		if (y & 1) result = LowLevelI64Add(result, x);
-		y >>= 1;
-		x <<= 1;
-	}
-	return result;
-}
-
-float LowLevelf32Mul(float x, float y)
-{
-	uint32_t I32x = *(uint32_t*)&x;
-	uint32_t I32y = *(uint32_t*)&y;
-	
-	uint64_t resultMantissa = LowLevelI64Mul((uint64_t)I32x & 0x7FFFFF | 0x800000, (uint64_t)I32y & 0x7FFFFF | 0x800000);
-	uint32_t resultExponent = LowLevelI32Add(LowLevelI32Add(I32x >> 23 & 0xFF, I32y >> 23 & 0xFF), -127);
-	bool roundUp = resultMantissa & 0x400000;
-	resultMantissa >>= 23;
-
-	//try a guard bit, a rounding bit
-	
-	while (resultMantissa & 0xffffffffff000000)
-	{
-		roundUp = resultMantissa & 1;
-		resultMantissa >>= 1;
-		resultExponent = LowLevelI32Add(resultExponent, 1);
-	}
-	resultMantissa = LowLevelI64Add(resultMantissa, roundUp);
-	
-	while (resultMantissa && !(resultMantissa & 0x800000))
-	{
-		resultMantissa <<= 1;
-		resultExponent = LowLevelI32Add(resultExponent, -1);
-	}
-	
-	uint32_t result = (I32x ^ I32y) & 0x80000000 | resultExponent << 23 | (resultMantissa & 0x7FFFFF);
-	
-	return *(float*)&result;
-}
-
-float LowLevelf32Add(float x, float y)
-{
-	uint32_t I32a = *(uint32_t*)&x;
-	uint32_t I32b = *(uint32_t*)&y;
-	
-	uint32_t xExponent = I32a >> 23 & 0xFF;
-	uint32_t yExponent = I32b >> 23 & 0xFF;
-
-	uint32_t xMantissa = I32a & 0x7FFFFF | 0x800000;
-	uint32_t yMantissa = I32b & 0x7FFFFF | 0x800000;
-
-	// try a guard bit, a rounding bit and one extra sticky bit
-
-	if (I32a & 0x80000000)
-		xMantissa = LowLevelI32Flip(xMantissa);
-	if (I32b & 0x80000000)
-		yMantissa = LowLevelI32Flip(yMantissa);
-
-	if (y > x)	// change
-	{
-		std::swap(I32a, I32b);
-		std::swap(xExponent, yExponent);
-		std::swap(xMantissa, yMantissa);
-	}
-
-	for (;;)
-	{
-		if (xExponent <= yExponent) break;
-		xMantissa >>= 1;
-		xExponent = LowLevelI32Add(xExponent, 1);
-		if (xExponent <= yExponent) break;
-		yMantissa <<= 1;
-		yExponent = LowLevelI32Add(yExponent, -1);
-	}
-
-	uint32_t resultMantissa = LowLevelI32Add(xMantissa, yMantissa);
-	uint32_t resultExponent = xExponent;
-	uint32_t resultSign = 0;
-
-	if (resultMantissa & 0x80000000)
-	{
-		resultSign = 0x80000000;
-		LowLevelI32Flip(resultMantissa);
-	}
-	
-	while (resultMantissa & 0xff000000)
-	{
-		resultMantissa >>= 1;
-		resultExponent = LowLevelI32Add(resultExponent, 1);
-	}
-
-	while (resultMantissa && !(resultMantissa & 0x800000))
-	{
-		resultMantissa <<= 1;
-		resultExponent = LowLevelI32Add(resultExponent, -1);
-	}
-	
-	uint32_t result = resultSign | resultExponent << 23 | (resultMantissa & 0x7FFFFF);
-	float answer = x + y;
-	uint32_t U32a = *(uint32_t*)&answer;
-
-	for (int32_t i = 31; i >= 0; i--)
-	{
-		printf("%d", (U32a >> i) & 1);
-		if (i == 31 || i == 23) printf(" ");
+		for (uint32_t j = 0; j < cols; j++)
+			printf("%8.3f ", __half2float(arr[i * cols + j]));
+		printf("\n");
 	}
 	printf("\n");
-
-	for (int32_t i = 31; i >= 0; i--)
-	{
-		printf("%d", (result >> i) & 1);
-		if (i == 31 || i == 23) printf(" ");
-	}
-	printf("\n");
-
-	return *(float*)&result;
 }
 
-uint32_t main()
+__global__ void CurandNormalizeF16(__half* output, uint32_t size, float min, float range)
 {
-	Random random(Random::MakeSeed(275));
-	for (uint32_t i = 10; i--;)
-	{
-		float x = random.Rfloat(-100, 100);
-		float y = random.Rfloat(-100, 100);
-		//printf("%f * %f = %f\n", x, y, LowLevelf32Mul(x, y));
-		printf("%f + %f = %f\n", x, y, LowLevelf32Add(x, y));
-		printf("%f + %f = %f\n\n", x, y, x + y);
-		/*int32_t x = (int32_t)random.Ruint32() * 0.00000001f;
-		printf("%d = %d\n", x, LowLevelI32Flip(x));*/
-	}
+	uint32_t index = blockIdx.x * blockDim.x + threadIdx.x;
+	if (index < size)
+		output[index] = __float2half(*(uint16_t*)(output + index) * range + min);
+}
+
+void CurandGenerateUniformF16(curandGenerator_t generator, __half* output, uint32_t size, float min = -1.0f, float max = 1.0f)
+{
+	curandGenerate(generator, (uint32_t*)output, (size >> 1) + (size & 1));
+	CurandNormalizeF16 << <std::ceil(0.0009765625f * size), 1024 >> > (output, size, min, (max - min) * 0.0000152590218967f);
+}
+
+int main()
+{
+	printf("cuDNN version: %d.%d.%d\n", CUDNN_MAJOR, CUDNN_MINOR, CUDNN_PATCHLEVEL);
+	printf("cuBLAS version: %d.%d.%d\n", CUBLAS_VER_MAJOR, CUBLAS_VER_MINOR, CUBLAS_VER_PATCH);
+	printf("cuRAND version: %d.%d.%d\n", CURAND_VERSION / 1000, (CURAND_VERSION % 1000) / 100, CURAND_VERSION % 100);
+	printf("\n");
+
+	cublasHandle_t cublasHandle;
+	cublasCreate(&cublasHandle);
+
+	cudnnHandle_t cudnnHandle;
+	cudnnCreate(&cudnnHandle);
+
+	curandGenerator_t curandGenerator;
+	curandCreateGenerator(&curandGenerator, CURAND_RNG_PSEUDO_DEFAULT);
+	curandSetPseudoRandomGeneratorSeed(curandGenerator, 1234ULL);
+
+	const float alpha = 1.0f;
+	const float beta = 0.0f;
+
+	const __half alphaf16 = __float2half(1.0f);
+	const __half betaf16 = __float2half(0.0f);
 	
+	const uint32_t N = 8;
+
+	__half* gpuArr1;
+	__half* gpuArr2;
+	
+	cudaMalloc(&gpuArr1, N * sizeof(__half));
+	cudaMalloc(&gpuArr2, N * sizeof(__half));
+	
+	__half* cpuArr1 = new __half[N];
+	__half* cpuArr2 = new __half[N];
+
+	CurandGenerateUniformF16(curandGenerator, gpuArr1, N);
+	cudaMemset(gpuArr2, 0, N * sizeof(__half));
+	
+	cudaMemcpy(cpuArr1, gpuArr1, N * sizeof(__half), cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpuArr2, gpuArr2, N * sizeof(__half), cudaMemcpyDeviceToHost);
+	
+	PrintMatrixF16(cpuArr1, 1, N, "Random");
+	PrintMatrixF16(cpuArr2, 1, N, "Zero");
+
+	/*
+	use:
+	cublasStatus_t cublasAxpyEx(cublasHandle_t handle,
+		int n,
+		const void* alpha,
+		cudaDataType alphaType,
+		const void* x,
+		cudaDataType xType,
+		int incx,
+		void* y,
+		cudaDataType yType,
+		int incy,
+		cudaDataType executiontype);
+	*/
+
+	cublasAxpyEx(cublasHandle, N, &alpha, CUDA_R_32F, gpuArr1, CUDA_R_16F, 1, gpuArr2, CUDA_R_16F, 1, CUDA_R_32F);
+	cudaMemcpy(cpuArr2, gpuArr2, N * sizeof(__half), cudaMemcpyDeviceToHost);
+	PrintMatrixF16(cpuArr2, 1, N, "Axpy");
+
 	return 0;
 }
