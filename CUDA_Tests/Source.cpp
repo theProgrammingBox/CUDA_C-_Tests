dbafa46c7242288bb2319e7e24fa9f13e5718762
theProgrammingBox <justinboxgea@gmail.com>
2023-03-26 21:53:22 -0700

    got strings to render well

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -23,6 +23,7 @@ public:
 		zoomLevel = 1.0f;
 		offset = { 0.0f, 0.0f };
 		halfScreen = { ScreenWidth() * 0.5f, ScreenHeight() * 0.5f };
+		lastMousePos = GetMousePos();
         
 		circles.emplace_back(100, 100, 10);
 		circles.emplace_back(-200, 200, 20);
@@ -34,29 +35,29 @@ public:
     
     bool OnUserUpdate(float fElapsedTime) override
     {
-		Controls(fElapsedTime);
+		Controls();
 		Render();
         
         return true;
     }
     
 private:
-	static constexpr float zoomSpeed = 4.0f;
+	static constexpr float zoomSpeed = 2.0f;
     static constexpr float panSpeed = 400.0f;
 
     float zoomLevel;
 	olc::vf2d offset;
 	olc::vf2d halfScreen;
+	olc::vf2d lastMousePos;
     std::vector<Circle> circles;
 
-    void Controls(float fElapsedTime)
+    void Controls()
     {
-        if (GetKey(olc::Key::Q).bHeld) zoomLevel *= std::pow(zoomSpeed, fElapsedTime);
-        if (GetKey(olc::Key::E).bHeld) zoomLevel *= std::pow(zoomSpeed, -fElapsedTime);
-		if (GetKey(olc::Key::A).bHeld) offset.x += panSpeed * fElapsedTime / zoomLevel;
-		if (GetKey(olc::Key::D).bHeld) offset.x -= panSpeed * fElapsedTime / zoomLevel;
-		if (GetKey(olc::Key::W).bHeld) offset.y += panSpeed * fElapsedTime / zoomLevel;
-		if (GetKey(olc::Key::S).bHeld) offset.y -= panSpeed * fElapsedTime / zoomLevel;
+		if (GetMouseWheel() > 0) zoomLevel *= std::pow(zoomSpeed, -0.1f);
+		if (GetMouseWheel() < 0) zoomLevel *= std::pow(zoomSpeed, 0.1f);
+		olc::vf2d delta = GetMousePos() - lastMousePos;
+        if (GetMouse(1).bHeld) offset += delta / zoomLevel;
+        lastMousePos = GetMousePos();
     }
 
     void Render()
@@ -69,13 +70,17 @@ private:
 			if (pos.x + radius > 0 && pos.x - radius < ScreenWidth() && pos.y + radius > 0 && pos.y - radius < ScreenHeight())
 				DrawCircle(pos, radius, olc::WHITE);
         }
+        
+		olc::vf2d stringPos = (olc::vf2d(10, 10) + offset) * zoomLevel + halfScreen;
+        float stringScale = 1.0f * zoomLevel;
+		DrawStringDecal(stringPos, "Zoom: " + std::to_string(zoomLevel), olc::WHITE, { stringScale, stringScale });
     }
 };
 
 int main()
 {
     ScrollAndPanProgram program;
-    if (program.Construct(800, 600, 1, 1))
+	if (program.Construct(1440, 810, 1, 1))
     {
         program.Start();
     }
