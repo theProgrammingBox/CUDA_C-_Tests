0cca7e92c67634386980ad34d5694c6e43b60bce
theProgrammingBox <justinboxgea@gmail.com>
2023-03-26 21:38:17 -0700

    working zoom and pan

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,96 +1,83 @@
-#include <cudnn.h>
-#include <cublas_v2.h>
-#include <curand.h>
-#include <cuda_fp16.h>
-#include <iostream>
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+#include <vector>
+#include <cmath>
 
-void PrintMatrixF16(__half* arr, uint32_t rows, uint32_t cols, const char* label)
-{
-	printf("%s:\n", label);
-	for (uint32_t i = 0; i < rows; i++)
-	{
-		for (uint32_t j = 0; j < cols; j++)
-			printf("%8.3f ", __half2float(arr[i * cols + j]));
-		printf("\n");
-	}
-	printf("\n");
-}
+class Circle {
+public:
+	olc::vf2d pos;
+    float radius;
+	Circle(float x, float y, float r) : pos(x, y), radius(r) {}
+};
 
-__global__ void CurandNormalizeF16(__half* output, uint32_t size, float min, float range)
+class ScrollAndPanProgram : public olc::PixelGameEngine
 {
-	uint32_t index = blockIdx.x * blockDim.x + threadIdx.x;
-	if (index < size)
-		output[index] = __float2half(*(uint16_t*)(output + index) * range + min);
-}
+public:
+    ScrollAndPanProgram() 
+    {
+        sAppName = "Scroll and Pan Program";
+    }
+    
+    bool OnUserCreate() override
+    {
+		zoomLevel = 1.0f;
+		offset = { 0.0f, 0.0f };
+		halfScreen = { ScreenWidth() * 0.5f, ScreenHeight() * 0.5f };
+        
+		circles.emplace_back(100, 100, 10);
+		circles.emplace_back(-200, 200, 20);
+		circles.emplace_back(300, -100, 15);
+		circles.emplace_back(-150, -250, 25);
 
-void CurandGenerateUniformF16(curandGenerator_t generator, __half* output, uint32_t size, float min = -1.0f, float max = 1.0f)
-{
-	curandGenerate(generator, (uint32_t*)output, (size >> 1) + (size & 1));
-	CurandNormalizeF16 << <std::ceil(0.0009765625f * size), 1024 >> > (output, size, min, (max - min) * 0.0000152590218967f);
-}
+		return true;
+    }
+    
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+		Controls(fElapsedTime);
+		Render();
+        
+        return true;
+    }
+    
+private:
+	static constexpr float zoomSpeed = 4.0f;
+    static constexpr float panSpeed = 400.0f;
+
+    float zoomLevel;
+	olc::vf2d offset;
+	olc::vf2d halfScreen;
+    std::vector<Circle> circles;
+
+    void Controls(float fElapsedTime)
+    {
+        if (GetKey(olc::Key::Q).bHeld) zoomLevel *= std::pow(zoomSpeed, fElapsedTime);
+        if (GetKey(olc::Key::E).bHeld) zoomLevel *= std::pow(zoomSpeed, -fElapsedTime);
+		if (GetKey(olc::Key::A).bHeld) offset.x += panSpeed * fElapsedTime / zoomLevel;
+		if (GetKey(olc::Key::D).bHeld) offset.x -= panSpeed * fElapsedTime / zoomLevel;
+		if (GetKey(olc::Key::W).bHeld) offset.y += panSpeed * fElapsedTime / zoomLevel;
+		if (GetKey(olc::Key::S).bHeld) offset.y -= panSpeed * fElapsedTime / zoomLevel;
+    }
+
+    void Render()
+    {
+		Clear(olc::BLACK);
+        for (auto& circle : circles)
+        {
+			olc::vf2d pos = (circle.pos + offset) * zoomLevel + halfScreen;
+			float radius = circle.radius * zoomLevel;
+			if (pos.x + radius > 0 && pos.x - radius < ScreenWidth() && pos.y + radius > 0 && pos.y - radius < ScreenHeight())
+				DrawCircle(pos, radius, olc::WHITE);
+        }
+    }
+};
 
 int main()
 {
-	printf("cuDNN version: %d.%d.%d\n", CUDNN_MAJOR, CUDNN_MINOR, CUDNN_PATCHLEVEL);
-	printf("cuBLAS version: %d.%d.%d\n", CUBLAS_VER_MAJOR, CUBLAS_VER_MINOR, CUBLAS_VER_PATCH);
-	printf("cuRAND version: %d.%d.%d\n", CURAND_VERSION / 1000, (CURAND_VERSION % 1000) / 100, CURAND_VERSION % 100);
-	printf("\n");
-
-	cublasHandle_t cublasHandle;
-	cublasCreate(&cublasHandle);
-
-	cudnnHandle_t cudnnHandle;
-	cudnnCreate(&cudnnHandle);
-
-	curandGenerator_t curandGenerator;
-	curandCreateGenerator(&curandGenerator, CURAND_RNG_PSEUDO_DEFAULT);
-	curandSetPseudoRandomGeneratorSeed(curandGenerator, 1234ULL);
-
-	const float alpha = 1.0f;
-	const float beta = 0.0f;
-
-	const __half alphaf16 = __float2half(1.0f);
-	const __half betaf16 = __float2half(0.0f);
-	
-	const uint32_t N = 8;
-
-	__half* gpuArr1;
-	__half* gpuArr2;
-	
-	cudaMalloc(&gpuArr1, N * sizeof(__half));
-	cudaMalloc(&gpuArr2, N * sizeof(__half));
-	
-	__half* cpuArr1 = new __half[N];
-	__half* cpuArr2 = new __half[N];
-
-	CurandGenerateUniformF16(curandGenerator, gpuArr1, N);
-	CurandGenerateUniformF16(curandGenerator, gpuArr2, N);
-	//cudaMemset(gpuArr2, 0, N * sizeof(__half));
-	
-	cudaMemcpy(cpuArr1, gpuArr1, N * sizeof(__half), cudaMemcpyDeviceToHost);
-	cudaMemcpy(cpuArr2, gpuArr2, N * sizeof(__half), cudaMemcpyDeviceToHost);
-	
-	PrintMatrixF16(cpuArr1, 1, N, "Random");
-	PrintMatrixF16(cpuArr2, 1, N, "Zero");
-
-	/*
-	use:
-	cublasStatus_t cublasAxpyEx(cublasHandle_t handle,
-		int n,
-		const void* alpha,
-		cudaDataType alphaType,
-		const void* x,
-		cudaDataType xType,
-		int incx,
-		void* y,
-		cudaDataType yType,
-		int incy,
-		cudaDataType executiontype);
-	*/
-
-	cublasAxpyEx(cublasHandle, N, &alpha, CUDA_R_32F, gpuArr1, CUDA_R_16F, 1, gpuArr2, CUDA_R_16F, 1, CUDA_R_32F);
-	cudaMemcpy(cpuArr2, gpuArr2, N * sizeof(__half), cudaMemcpyDeviceToHost);
-	PrintMatrixF16(cpuArr2, 1, N, "Axpy");
-
-	return 0;
+    ScrollAndPanProgram program;
+    if (program.Construct(800, 600, 1, 1))
+    {
+        program.Start();
+    }
+    return 0;
 }
