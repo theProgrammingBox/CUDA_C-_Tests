f378e6e49ea6b89a61b72b0e1fbd786351fecc12
theProgrammingBox <justinboxgea@gmail.com>
2023-03-27 15:06:50 -0700

    working on forms

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,13 +1,52 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <vector>
-#include <cmath>
+#include <memory>
 
-class Circle {
+class Element
+{
 public:
-	olc::vf2d pos;
-    float radius;
-	Circle(float x, float y, float r) : pos(x, y), radius(r) {}
+	olc::PixelGameEngine* pge;
+	Element(olc::PixelGameEngine* p) : pge(p) {}
+	
+	virtual void Render(const olc::vf2d& position, const olc::vf2d& size, const float& zoomLevel, const olc::vf2d& offset, const olc::vf2d& halfScreen) = 0;
+};
+
+class HueSlider : public Element
+{
+public:
+	float hue;
+
+	HueSlider(olc::PixelGameEngine* p) : Element(p), hue(0.0f) {}
+
+	void Render(const olc::vf2d& position, const olc::vf2d& size, const float& zoomLevel, const olc::vf2d& offset, const olc::vf2d& halfScreen)
+	{
+		pge->FillRectDecal((position + offset) * zoomLevel + halfScreen, size * zoomLevel, olc::PixelF(hue, 1.0f, 1.0f));
+	}
+};
+
+class Form
+{
+public:
+	olc::PixelGameEngine* pge;
+	olc::vf2d position;
+	olc::vf2d size;
+	std::string formName;
+    uint32_t numberOfElements;
+	std::unique_ptr<std::unique_ptr<Element>[]> element;
+
+	Form(olc::PixelGameEngine* p, uint32_t numberOfElements = 1) : pge(p), position(olc::vf2d(0, 0)), size(olc::vf2d(100, 100)), formName("Form"), numberOfElements(numberOfElements)
+	{
+		element = std::make_unique<std::unique_ptr<Element>[]>(numberOfElements);
+
+		for (uint32_t i = 0; i < numberOfElements; i++)
+			element[i] = std::make_unique<HueSlider>(pge);
+	}
+
+	void Render(const float& zoomLevel, const olc::vf2d& offset, const olc::vf2d& halfScreen)
+	{
+		pge->FillRectDecal((position + offset) * zoomLevel + halfScreen, size * zoomLevel, olc::WHITE);
+	}
 };
 
 class ScrollAndPanProgram : public olc::PixelGameEngine
@@ -25,10 +64,7 @@ public:
 		halfScreen = { ScreenWidth() * 0.5f, ScreenHeight() * 0.5f };
 		lastMousePos = GetMousePos();
         
-		circles.emplace_back(100, 100, 10);
-		circles.emplace_back(-200, 200, 20);
-		circles.emplace_back(300, -100, 15);
-		circles.emplace_back(-150, -250, 25);
+		forms.push_back(std::make_shared<Form>(this, 1));
 
 		return true;
     }
@@ -49,7 +85,7 @@ private:
 	olc::vf2d offset;
 	olc::vf2d halfScreen;
 	olc::vf2d lastMousePos;
-    std::vector<Circle> circles;
+	std::vector<std::shared_ptr<Form>> forms;
 
     void Controls()
     {
@@ -63,15 +99,12 @@ private:
     void Render()
     {
 		Clear(olc::BLACK);
-        for (auto& circle : circles)
-        {
-			olc::vf2d pos = (circle.pos + offset) * zoomLevel + halfScreen;
-			float radius = circle.radius * zoomLevel;
-			if (pos.x + radius > 0 && pos.x - radius < ScreenWidth() && pos.y + radius > 0 && pos.y - radius < ScreenHeight())
-				DrawCircle(pos, radius, olc::WHITE);
-        }
+		for (auto& form : forms)
+		{
+			form->Render(zoomLevel, offset, halfScreen);
+		}
         
-		olc::vf2d stringPos = (olc::vf2d(10, 10) + offset) * zoomLevel + halfScreen;
+		olc::vf2d stringPos = (olc::vf2d(100, 100) + offset) * zoomLevel + halfScreen;
         float stringScale = 1.0f * zoomLevel;
 		DrawStringDecal(stringPos, "Zoom: " + std::to_string(zoomLevel), olc::WHITE, { stringScale, stringScale });
     }
