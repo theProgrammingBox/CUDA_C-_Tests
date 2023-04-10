2415d97bb8124920e7c2914532652888c4b3a84a
theProgrammingBox <justinboxgea@gmail.com>
2023-04-09 23:14:55 -0700

    added log hack. working on exp hack

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <chrono>
 
 void randomNormalSetup(uint32_t kn[128], float fn[128], float wn[128])
@@ -10,21 +10,19 @@ void randomNormalSetup(uint32_t kn[128], float fn[128], float wn[128])
 
     kn[0] = dn / q * m1;
     kn[1] = 0;
-
     wn[0] = q / m1;
     wn[127] = dn / m1;
-
     fn[0] = 1.0;
     fn[127] = exp(-0.5 * dn * dn);
 
-    double tn = 3.442619855899;
+    double tn;
     for (uint8_t i = 126; 1 <= i; i--)
     {
+        tn = dn;
         dn = sqrt(-2.0 * log(vn / dn + exp(-0.5 * dn * dn)));
         kn[i + 1] = dn / tn * m1;
         fn[i] = exp(-0.5 * dn * dn);
         wn[i] = dn / m1;
-        tn = dn;
     }
 }
 
@@ -32,6 +30,7 @@ float randomNormal(uint32_t& seed, const uint32_t kn[128], const float fn[128], 
 {
     uint32_t tempSeed;
     int32_t randomInt;
+    int32_t expHack;
     uint8_t sevenBits;
     float x, y;
 
@@ -54,16 +53,22 @@ float randomNormal(uint32_t& seed, const uint32_t kn[128], const float fn[128], 
                 seed = (seed ^ (seed << 13));
                 seed = (seed ^ (seed >> 17));
                 seed = (seed ^ (seed << 5));
-                x = -0.2904764f * log((tempSeed + seed) * 2.32830643654e-10f);
+                x = (tempSeed + seed) * 2.32830643654e-10f;
+				x = (*(int32_t*)&x - 0x3f800000) * -2.3935259956e-8f;
 
                 tempSeed = seed;
                 seed = (seed ^ (seed << 13));
                 seed = (seed ^ (seed >> 17));
                 seed = (seed ^ (seed << 5));
-                y = -log((tempSeed + seed) * 2.32830643654e-10f);
+				y = (tempSeed + seed) * 2.32830643654e-10f;
+				y = (*(int32_t*)&y - 0x3f800000) * -8.24e-8f;
 
                 if (x * x <= y + y)
-                    return ((randomInt & 0x2) - 1) * (3.442620f + x);
+                {
+					x += 3.442620f;
+                    tempSeed = *(uint32_t*)&x ^ randomInt & 0x80000000;
+                    return *(float*)&tempSeed;
+                }
             }
         }
 
@@ -72,14 +77,39 @@ float randomNormal(uint32_t& seed, const uint32_t kn[128], const float fn[128], 
         seed = (seed ^ (seed << 13));
         seed = (seed ^ (seed >> 17));
         seed = (seed ^ (seed << 5));
+        /*int expHack = (int(x * 12135920) + 0x3f800000);
+        erravg += abs(exp(x) - *(float*)&expHack);*/
         if (fn[sevenBits] + (tempSeed + seed) * 2.32830643654e-10f * (fn[sevenBits - 1] - fn[sevenBits]) < exp(-0.5f * x * x))
             return x;
+        /*expHack = int32_t(12135920 * -0.5 * x * x) + 0x3f800000;
+        if (fn[sevenBits] + (tempSeed + seed) * 2.32830643654e-10f * (fn[sevenBits - 1] - fn[sevenBits]) < *(float*)&expHack)
+            return x;*/
     }
 }
 
 int main()
 {
-	uint32_t seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	/*uint32_t tempSeed;
+	float erravg = 0.0f;
+	const uint32_t samples = 100000000;
+    uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
+    
+    for (uint32_t i = samples; i--;)
+    {
+        tempSeed = seed;
+        seed = (seed ^ (seed << 13));
+        seed = (seed ^ (seed >> 17));
+        seed = (seed ^ (seed << 5));
+        float x = (seed + tempSeed) * 2.32830643654e-10f;
+
+        int expHack = (int(x * 12135920) + 0x3f800000);
+        erravg += abs(exp(x) - *(float*)&expHack);
+    }
+	printf("%f\n", erravg / samples);
+    
+    return 0;*/
+    
+	uint32_t seed = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 	uint32_t kn[128];
 	float fn[128];
 	float wn[128];
@@ -88,12 +118,12 @@ int main()
 
     const uint32_t bins = 56;
 	const uint32_t samples = 1000000;
-    uint32_t hist[bins];
-	float min = -3.0f;
-	float max = 3.0f;
-	float bin_width = (max - min) / bins;
     const float scale = 1000.0f / samples;
+	const float min = -3.0f;
+	const float max = 3.0f;
+	const float bin_width = (max - min) / bins;
     
+    uint32_t hist[bins];
 	memset(hist, 0, sizeof(hist));
 	for (uint32_t i = 0; i < samples; i++)
 	{
@@ -102,12 +132,14 @@ int main()
 			hist[bin]++;
 	}
     
+	printf("----------------------------------------\n");
 	for (uint32_t i = 0; i < bins; i++)
 	{
 		for (uint32_t j = scale * hist[i]; j--;)
 			printf("*");
 		printf("\n");
 	}
+	printf("----------------------------------------\n");
 
 	return 0;
 }
