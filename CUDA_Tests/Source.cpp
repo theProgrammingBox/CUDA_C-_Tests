a28918f9745c618c35bd5c3b00bdbb9ffee90893
theProgrammingBox <justinboxgea@gmail.com>
2023-04-19 16:05:22 -0700

    ok, i think this is random enough

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,16 +1,16 @@
 ﻿#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-uint32_t Lehmer32(uint64_t seed, uint64_t subsequence = 0)
+uint32_t lcg_parkmiller(uint32_t seed)
 {
-	uint64_t tmp = (seed + 0xe120fc15) * 0x4a39b70d;
-	tmp = (tmp >> 32 ^ tmp) * 0x12fad5c9;
-	return tmp >> 32 ^ tmp;
+	return seed * 279470273u % 0xfffffffb;
 }
 
 class Example : public olc::PixelGameEngine
 {
 public:
+	uint64_t seed;
+	
 	Example()
 	{
 		sAppName = "Example";
@@ -18,21 +18,30 @@ public:
 	
 	bool OnUserCreate() override
 	{
-		float ratio = 0;
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-			{
-				bool bPixel = Lehmer32(x * ScreenHeight() + y) & 3;
-				ratio += bPixel;
-				Draw(x, y, bPixel ? olc::BLACK : olc::WHITE);
-			}
-		printf("Ratio: %f\n", ratio / (ScreenWidth() * ScreenHeight()));
+		seed = 0;
+		rng();
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		seed++;
+		rng();
 		return true;
+	}
+
+	void rng()
+	{
+		uint64_t subsequence = 0;
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+			{
+				uint64_t tmp = (seed + 0xe120fc15 ^ subsequence * 0x12fad5c9) * 0x4a39b70d;
+				tmp = (tmp >> 32 ^ tmp) * 0x12fad5c9;
+				float bPixel = uint32_t(tmp >> 32 ^ tmp) * 2.3283064365386963e-10f;
+				subsequence++;
+				Draw(x, y, olc::PixelF(bPixel, bPixel, bPixel));
+			}
 	}
 };
 
