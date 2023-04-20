a9ebbb0686301bc30a4736ec1e1c6a5d1cd15ad8
theProgrammingBox <justinboxgea@gmail.com>
2023-04-19 17:07:47 -0700

    researching better ones

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -25,6 +25,7 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		//seed += ScreenHeight() * ScreenWidth();
 		seed++;
 		rng();
 		return true;
@@ -36,11 +37,12 @@ public:
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
 			{
-				uint64_t tmp = (seed + 0xe120fc15 ^ subsequence * 0x12fad5c9) * 0x4a39b70d;
+				uint64_t tmp = (subsequence ^ 0xe120fc15) * 0x4a39b70d;
+				tmp = (tmp >> 32 ^ tmp ^ (seed ^ 0xe120fc15) * 0x4a39b70d) * 0x12fad5c9;
 				tmp = (tmp >> 32 ^ tmp) * 0x12fad5c9;
-				float bPixel = uint32_t(tmp >> 32 ^ tmp) * 2.3283064365386963e-10f;
+				tmp ^= tmp >> 32;
 				subsequence++;
-				Draw(x, y, olc::PixelF(bPixel, bPixel, bPixel));
+				Draw(x, y, olc::PixelF(tmp & 0x1, tmp & 0x1, tmp & 0x1));
 			}
 	}
 };
