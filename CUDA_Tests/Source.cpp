15725d6968994baac25adfefb12ef02e0b2ba780
theProgrammingBox <justinboxgea@gmail.com>
2023-04-19 18:21:58 -0700

    final change

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,15 +1,10 @@
 ﻿#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-uint32_t lcg_parkmiller(uint32_t seed)
-{
-	return seed * 279470273u % 0xfffffffb;
-}
-
 class Example : public olc::PixelGameEngine
 {
 public:
-	uint64_t seed;
+	uint64_t seed2;
 	
 	Example()
 	{
@@ -18,35 +13,34 @@ public:
 	
 	bool OnUserCreate() override
 	{
-		seed = 0;
+		seed2 = 0;
 		rng();
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		//seed += ScreenHeight() * ScreenWidth();
-		seed++;
+		seed2++;
 		rng();
 		return true;
 	}
 
 	void rng()
 	{
-		uint64_t subsequence = 0;
+		uint64_t seed1 = 0;
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
 			{
-				uint32_t h = subsequence * 0xcc9e2d51;
+				uint32_t h = seed1 * 0xcc9e2d51;
 				h ^= (h << 15) | (h >> 17);
 				h *= 0x1b873593;
 				h ^= (h << 13) | (h >> 19);
-				h ^= seed * 0xcc9e2d51;
+				h ^= seed2 * 0xcc9e2d51;
 				h ^= (h << 15) | (h >> 17);
 				h *= 0x1b873593;
 				h ^= (h << 13) | (h >> 19);
 				
-				subsequence++;
+				seed1++;
 				Draw(x, y, olc::PixelF(h & 0xff, h >> 8 & 0xff, h >> 16 & 0xff));
 			}
 	}
