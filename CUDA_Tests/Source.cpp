f95fc406cd29ccf7ce65c5451546c6b326f05a5b
theProgrammingBox <justinboxgea@gmail.com>
2023-04-25 21:18:38 -0700

    playing around with some hashing ideas

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -25,9 +25,22 @@ public:
 		for (int x = 0; x < ScreenWidth(); ++x)
 			for (int y = 0; y < ScreenHeight(); ++y)
 			{
-				uint32_t hash = (idx + offset ^ seed) * 0xAA69E974;
-				hash = (hash >> 13 ^ hash) * 0x8B7A1B65;
-				Draw(x, y, olc::PixelF(hash & 0xff, (hash >> 8) & 0xff, (hash >> 16) & 0xff));
+				uint8_t* idx2 = (uint8_t*)&idx;
+				uint8_t* offset2 = (uint8_t*)&offset;
+				uint8_t* seed2 = (uint8_t*)&seed;
+
+				uint8_t arr[4];
+				arr[0] = (idx2[1] * offset2[3] ^ seed2[2] ^ 0xA8);
+				arr[1] = (idx2[3] ^ offset2[1] * seed2[0] ^ 0x83);
+				arr[2] = (idx2[0] * offset2[2] ^ seed2[3] ^ 0x59);
+				arr[3] = (idx2[2] ^ offset2[0] * seed2[1] ^ 0x63);
+
+				arr[0] ^= arr[1] * arr[2] >> 3;
+				arr[1] ^= arr[3] * arr[0] >> 3;
+				arr[2] ^= arr[0] * arr[3] >> 3;
+				arr[3] ^= arr[2] * arr[1] >> 3;
+
+				Draw(x, y, olc::PixelF(arr[0], arr[1], arr[2]));
 				++idx;
 			}
 		seed += 0xA8835963;
