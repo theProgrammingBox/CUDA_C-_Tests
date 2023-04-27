0ff46d5558081f463b5922859fac4d0bca0c1ea6
theProgrammingBox <justinboxgea@gmail.com>
2023-04-26 17:07:13 -0700

    this performs better but can only produce 1 number

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -4,62 +4,68 @@
 #include <time.h>
 #include <iostream>
 
-#define NUM_SAMPLES 1000000
+#define NUM_SAMPLES 40000000
 #define NUM_BINS 100
 
-double uniform_random_range(double min, double max) {
-    double u = (double)rand() / (double)RAND_MAX;
-    return min + u * (max - min);
+float uniform_random_range() {
+	const float scale = 1.0f / RAND_MAX;
+    float r = rand() | 1;
+    return r * scale;
 }
 
-double marsaglia_polar_s() {
-    /*double u, v, s;
-    do {
-        u = uniform_random_range(-1.0, 1.0);
-        v = uniform_random_range(-1.0, 1.0);
-        s = u * u + v * v;
-    } while (s >= 1.0 || s == 0.0);*/
-    /*double factor = sqrt(-2.0 * log(s) / s);
-    return u * factor * 0.1f + 0.5f;*/
+//double marsaglia_polar_s() {
+//    double u, v, s;
+//    do {
+//        u = uniform_random_range(-1.0, 1.0);
+//        v = uniform_random_range(-1.0, 1.0);
+//        s = u * u + v * v;
+//    } while (s >= 1.0 || s == 0.0);
+//    double factor = sqrt(-2.0 * log(s) / s);
+//    return u * factor * 0.08f + 0.5f;
+//}
 
-    /*s = uniform_random_range(0.0, 1.0);
-    double factor = sqrt(-2.0 * log(s) / s);
-    return uniform_random_range(-1.0, 1.0) * factor * 0.1f + 0.5f;*/
-    /*do {
-        u = uniform_random_range(-1.0, 1.0);
-        v = uniform_random_range(-1.0, 1.0);
-        s = u * u + v * v;
-    } while (s >= 1.0 || s == 0.0);*/
-    //return u * 0.5 + 0.5;
-    //s = uniform_random_range(0.0, 1.0);
-    //return u * sqrt(-2 * logf(s) / s) * 0.2 + 0.5;
-    float u = sqrt(uniform_random_range(0.0, 1.0));
-    float v = sqrt(uniform_random_range(0.0, 1.0));
-    float a = sqrt(uniform_random_range(0.0, 1.0));
-    float b = sqrt(uniform_random_range(0.0, 1.0));
-    return (v + u - a - b) * 0.2 + 0.5;
+double box_muller() {
+	double u = uniform_random_range();
+	double v = uniform_random_range();
+	double factor = sqrt(-2.0 * log(u));
+	return cos(6.28318530718 * v) * factor * 0.08f + 0.5f;
+}
+
+double custom() {
+    float a = sqrtf(-logf(uniform_random_range()));
+    float b = sqrtf(-logf(uniform_random_range()));
+    
+    return ((a - b) * 1.54) * 0.08 + 0.5;
 }
 
 int main() {
     srand(time(NULL));
 
-    uint32_t histogram[NUM_BINS] = { 0 };
-    float scale = (float)NUM_BINS / NUM_SAMPLES * 16;
+    uint32_t histogram1[NUM_BINS] = { 0 };
+	uint32_t histogram2[NUM_BINS] = { 0 };
+    float scale = (float)NUM_BINS / NUM_SAMPLES * 18;
 
+    double s;
+    int bin_index;
     for (int i = 0; i < NUM_SAMPLES; i++) {
-        double s = marsaglia_polar_s();
-        int bin_index = (int)(s * NUM_BINS);
-        if (bin_index < NUM_BINS && bin_index >= 0) {
-            histogram[bin_index]++;
-		}
+        s = box_muller();
+        bin_index = (int)(s * NUM_BINS);
+        histogram1[bin_index]++;
+        
+        s = custom();
+		bin_index = (int)(s * NUM_BINS);
+        histogram2[bin_index]++;
     }
 
     printf("Histogram of s values:\n");
     printf("Bin\tFrequency\n");
 
+    float diff;
     for (int i = 0; i < NUM_BINS; i++) {
         printf("%u\t", i);
-        for (int j = 0; j < histogram[i] * scale; j++) {
+		diff = (float)histogram2[i] / histogram1[i] * 100;
+		printf("%f%%\t", diff);
+        for (int j = 0; j < int(histogram2[i] * scale + 1); j++) {
 			printf("*");
 		}
         printf("\n");
