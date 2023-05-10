e1ed96b858287ab2e5447efbbdf2724e9fbb90ac
theProgrammingBox <justinboxgea@gmail.com>
2023-05-10 15:19:37 -0700

    corrected coloring and limit math

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -5,7 +5,9 @@
 class Example : public olc::PixelGameEngine
 {
 public:
+	const float reward = 1;
 	const float discount = 0.99f;
+	const float limit = reward / (1 - discount);
 	static const int samples = 1800;
 	float rewards[samples];
 	int idx;
@@ -24,48 +26,40 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// clear screen
 		Clear(olc::BLACK);
-
-		// if up, add 1 reward to front, if down, add -1 reward to front
+		
 		if (GetKey(olc::Key::UP).bHeld)
 		{
-			rewards[idx] = 1;
+			rewards[idx] = reward;
 		}
 		else if (GetKey(olc::Key::DOWN).bHeld)
 		{
-			rewards[idx] = -1;
+			rewards[idx] = -reward;
 		}
 		else
 		{
 			rewards[idx] = 0;
 		}
-
-		// calculate discount reward
-		float discount_reward = rewards[idx];
-
-		//cycle the arr
-		idx--;
-		idx += (idx < 0) * samples;
-
+		
+		float discount_reward = 0;
 		for (int i = samples; i--;)
 		{
-			idx++;
-			idx *= idx < samples;
 
 			discount_reward = rewards[idx] + discount * discount_reward;
-			// red if negative, green if positive, color gradient
-			// red 255 - 255 - 0
-			// green 0 - 255 - 255
-			const float limit = 200.0f;
-			float red = 255 * std::max(-discount_reward / limit, 0.0f);
-			float green = 255 * std::max(discount_reward / limit, 0.0f);
-			olc::Pixel p = olc::Pixel(red, green, 0);
-			Draw(i, 400 - discount_reward, p);
-			//printf("%f\n", discount_reward);
+			
+			int red = 255 * std::min(std::max(-discount_reward / limit + 1.0f, 0.0f), 1.0f);
+			int green = 255 * std::min(std::max(discount_reward / limit + 1.0f, 0.0f), 1.0f);
+			int blue = 255 * std::max(1 - abs(discount_reward / limit), 0.0f);
+			olc::Pixel color = olc::Pixel(red, green, blue);
+			
+			Draw(i, ScreenHeight() / 2 - discount_reward, color);
+			
+			idx++;
+			idx *= idx < samples;
 		}
-
-
+		idx--;
+		idx += (idx < 0) * samples;
+		
 		return true;
 	}
 };
