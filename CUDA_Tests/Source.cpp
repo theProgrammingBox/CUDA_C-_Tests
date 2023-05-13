b8c3712bbe4e04024f9692aec2c4eeab2bef8010
theProgrammingBox <justinboxgea@gmail.com>
2023-05-13 16:32:25 -0700

    testing

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,58 +1,40 @@
-﻿#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
+﻿#include <iostream>
+#include <assert.h>
 
-class Example : public olc::PixelGameEngine
+struct Param4D
 {
-public:
-	const float reward = 1;
-	const float discount = 0.99f;
-	const float invLimit = (1 - discount) / reward;
-	static const int samples = 1800;
-	float rewards[samples];
-	int idx;
-	float middle;
+	uint32_t height;
+	uint32_t width;
+	uint32_t channels;
+	uint32_t batches;
 
-	bool OnUserCreate() override
+	Param4D(uint32_t height = 1, uint32_t width = 1, uint32_t channels = 1, uint32_t batches = 1)
 	{
-		memset(rewards, 0, sizeof(rewards));
-		idx = 0;
-		middle = ScreenHeight() * 0.5f;
-		return true;
+		assert(height > 0 && width > 0 && channels > 0 && batches > 0);
+		this->height = height;
+		this->width = width;
+		this->channels = channels;
+		this->batches = batches;
 	}
 
-	bool OnUserUpdate(float fElapsedTime) override
+	Param4D(const Param4D* other)
 	{
-		Clear(olc::BLACK);
+		this->height = other->height;
+		this->width = other->width;
+		this->channels = other->channels;
+		this->batches = other->batches;
+	}
 
-		rewards[idx] = (GetKey(olc::Key::UP).bHeld - GetKey(olc::Key::DOWN).bHeld) * reward;
-		
-		float discount_reward = 0;
-		for (int i = samples; i--;)
-		{
-			discount_reward = rewards[idx] + discount * discount_reward;
-			
-			float norm = discount_reward * invLimit;
-			int red = 255 * std::max(std::min(1.0f - norm, 1.0f), 0.0f);
-			int green = 255 * std::max(std::min(1.0f + norm, 1.0f), 0.0f);
-			int blue = 255 * std::max(1.0f - abs(norm), 0.0f);
-			olc::Pixel color = olc::Pixel(red, green, blue);
-			
-			Draw(i, middle - discount_reward, color);
-			
-			idx++;
-			idx *= idx < samples;
-		}
-		idx--;
-		idx += (idx < 0) * samples;
-		
-		return true;
+	void Print() const
+	{
+		printf("(%u, %u, %u, %u)\n", height, width, channels, batches);
 	}
 };
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(1800, 800, 1, 1))
-		demo.Start();
-	return 0;
+	Param4D param = { 64 };
+	Param4D* parameter = new Param4D(param);
+	Param4D* parameter2 = new Param4D(parameter);
+	parameter->Print();
 }
