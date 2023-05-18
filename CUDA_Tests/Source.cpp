0d83ae7f0221db3f38a2b39fa48390f547892921
theProgrammingBox <justinboxgea@gmail.com>
2023-05-17 17:29:22 -0700

    testing save

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -3,16 +3,7 @@
 
 int main()
 {
-public:
-	const float reward = 1;
-	const float discount = 0.99f;
-	const float invLimit = (1 - discount) / reward;
-	static const int samples = 1800;
-	float rewards[samples];
-	int idx;
-	float middle;
-
-	bool OnUserCreate() override
+	if (false)
 	{
 		std::ofstream file("data.bin", std::ios::binary);
 		if (!file)
@@ -23,7 +14,7 @@ public:
 
 		bool b = true;
 		void* ptr = reinterpret_cast<void*>(0x12345678);  // Just an example
-		
+
 		for (int i = 0; i < 10; i++)
 		{
 			file.write(reinterpret_cast<const char*>(&b), sizeof(b));
@@ -38,29 +29,16 @@ public:
 			return 1;
 		}
 
-		rewards[idx] = (GetKey(olc::Key::UP).bHeld - GetKey(olc::Key::DOWN).bHeld) * reward;
-		
-		float discount_reward = 0;
-		for (int i = samples; i--;)
+		bool b;
+		void* ptr;
+
+		while (file.read(reinterpret_cast<char*>(&b), sizeof(b)) &&
+			file.read(reinterpret_cast<char*>(&ptr), sizeof(ptr)))
 		{
-			discount_reward = rewards[idx] + discount * discount_reward;
-			
-			float norm = discount_reward * invLimit;
-			int red = 255 * std::max(std::min(1.0f - norm, 1.0f), 0.0f);
-			int green = 255 * std::max(std::min(1.0f + norm, 1.0f), 0.0f);
-			int blue = 255 * std::max(1.0f - abs(norm), 0.0f);
-			olc::Pixel color = olc::Pixel(red, green, blue);
-			
-			Draw(i, middle - discount_reward, color);
-			
-			idx++;
-			idx *= idx < samples;
+			std::cout << "Bool: " << std::boolalpha << b << "\n";
+			std::cout << "Address: " << ptr << "\n";
 		}
-		idx--;
-		idx += (idx < 0) * samples;
-		
-		return true;
 	}
 
-    return 0;
+	return 0;
 }
