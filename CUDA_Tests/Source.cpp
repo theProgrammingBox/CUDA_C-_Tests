32de5fd09eebebd8a9987522107b8a2c234c25e2
theProgrammingBox <justinboxgea@gmail.com>
2023-05-17 17:28:15 -0700

    testing saving system

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,58 +1,44 @@
-﻿#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
-
-class Example : public olc::PixelGameEngine
-{
-public:
-	const float reward = 1;
-	const float discount = 0.99f;
-	const float invLimit = (1 - discount) / reward;
-	static const int samples = 1800;
-	float rewards[samples];
-	int idx;
-	float middle;
-
-	bool OnUserCreate() override
-	{
-		memset(rewards, 0, sizeof(rewards));
-		idx = 0;
-		middle = ScreenHeight() * 0.5f;
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		Clear(olc::BLACK);
-
-		rewards[idx] = (GetKey(olc::Key::UP).bHeld - GetKey(olc::Key::DOWN).bHeld) * reward;
-		
-		float discount_reward = 0;
-		for (int i = samples; i--;)
-		{
-			discount_reward = rewards[idx] + discount * discount_reward;
-			
-			float norm = discount_reward * invLimit;
-			int red = 255 * std::max(std::min(1.0f - norm, 1.0f), 0.0f);
-			int green = 255 * std::max(std::min(1.0f + norm, 1.0f), 0.0f);
-			int blue = 255 * std::max(1.0f - abs(norm), 0.0f);
-			olc::Pixel color = olc::Pixel(red, green, blue);
-			
-			Draw(i, middle - discount_reward, color);
-			
-			idx++;
-			idx *= idx < samples;
-		}
-		idx--;
-		idx += (idx < 0) * samples;
-		
-		return true;
-	}
-};
+﻿#include <iostream>
+#include <fstream>
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(1800, 800, 1, 1))
-		demo.Start();
-	return 0;
+	if (false)
+	{
+		std::ofstream file("data.bin", std::ios::binary);
+		if (!file)
+		{
+			std::cerr << "Couldn't open file for writing.\n";
+			return 1;
+		}
+
+		bool b = true;
+		void* ptr = reinterpret_cast<void*>(0x12345678);  // Just an example
+		
+		for (int i = 0; i < 10; i++)
+		{
+			file.write(reinterpret_cast<const char*>(&b), sizeof(b));
+			file.write(reinterpret_cast<const char*>(&ptr), sizeof(ptr));
+		}
+	}
+	else
+	{
+		std::ifstream file("data.bin", std::ios::binary);
+		if (!file) {
+			std::cerr << "Couldn't open file for reading.\n";
+			return 1;
+		}
+
+		bool b;
+		void* ptr;
+		
+		while (file.read(reinterpret_cast<char*>(&b), sizeof(b)) &&
+			file.read(reinterpret_cast<char*>(&ptr), sizeof(ptr)))
+		{
+			std::cout << "Bool: " << std::boolalpha << b << "\n";
+			std::cout << "Address: " << ptr << "\n";
+		}
+	}
+
+    return 0;
 }
