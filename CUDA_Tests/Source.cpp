500ab2b580e4fc4206d0846fe0a652b0423437b7
theProgrammingBox <justinboxgea@gmail.com>
2023-07-01 07:36:25 -0700

    s

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,55 +1,83 @@
 ﻿#include <iostream>
-#include <vector>
-
-struct Layer
-{
-	int size;
-	float* arr;
-	
-	Layer(int size) : size(size)
-	{
-		arr = new float[size];
-	}
-
-	~Layer()
-	{
-		delete[] arr;
-	}
-};
-
-struct SomeLayer : Layer
-{
-	SomeLayer(int size) : Layer(size)
-	{
-		for (int i = 0; i < size; i++)
-		{
-			arr[i] = 0.0f;
-		}
-	}
-};
-
-struct Network
-{
-	std::vector<Layer*> layers;
-
-	~Network()
-	{
-		for (auto layer : layers)
-		{
-			delete layer;
-		}
-	}
-
-	void AddLayer(Layer* layer)
-	{
-		layers.emplace_back(layer);
-	}
-};
+#include <fstream>
+#include <unordered_set>
 
 int main()
 {
-	Network net;
-	net.AddLayer(new SomeLayer(10));
-	
+
+	std::ofstream file("data.bin", std::ios::binary);
+	if (!file)
+	{
+		std::cerr << "Couldn't open file for writing.\n";
+		return 1;
+	}
+
+	bool b;
+	float* ptr;
+
+	for (int i = 0; i < 10; i++)
+	{
+		ptr = new float(i);
+
+		b = 1;
+		file.write(reinterpret_cast<const char*>(&b), sizeof(b));
+		file.write(reinterpret_cast<const char*>(&ptr), sizeof(ptr));
+		printf("Inserting address: %p\n", ptr);
+
+		b = 0;
+		file.write(reinterpret_cast<const char*>(&b), sizeof(b));
+		file.write(reinterpret_cast<const char*>(&ptr), sizeof(ptr));
+		delete ptr;
+		printf("Erasing address: %p\n", ptr);
+	}
+	file.close();
+	printf("\n\n\n");
+
+	std::ifstream file2("data.bin", std::ios::binary);
+	if (!file2)
+	{
+		std::cerr << "Couldn't open file for reading.\n";
+		return 1;
+	}
+
+	std::unordered_set<void*> addressSet;
+
+	while (file2.read(reinterpret_cast<char*>(&b), sizeof(b)) &&
+		file2.read(reinterpret_cast<char*>(&ptr), sizeof(ptr)))
+	{
+		if (b)
+		{
+			addressSet.insert(ptr);
+			printf("Inserting address: %p\n", ptr);
+		}
+		else
+		{
+			if (addressSet.find(ptr) == addressSet.end())
+			{
+				printf("Double delete on address: %p\n", ptr);
+			}
+			else
+			{
+				addressSet.erase(ptr);
+				printf("Erasing address: %p\n", ptr);
+			}
+		}
+	}
+	file2.close();
+	printf("\n\n\n");
+
+	if (addressSet.empty())
+	{
+		printf("Perfect\n");
+	}
+	else
+	{
+		printf("Leftover\n");
+		for (auto& ptr : addressSet)
+		{
+			printf("Leftover address: %p\n", ptr);
+		}
+	}
+
 	return 0;
 }
