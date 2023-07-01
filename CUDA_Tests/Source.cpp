5f4b1ecb382d78792061188a9e14c0398d349a15
theProgrammingBox <justinboxgea@gmail.com>
2023-07-01 07:36:54 -0700

    why wont you crash?

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,59 +1,55 @@
-﻿#define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
+﻿#include <iostream>
+#include <vector>
 
-class Example : public olc::PixelGameEngine
+struct Layer
 {
-public:
+	int size;
+	float* arr;
 
-	Example()
+	Layer(int size) : size(size)
 	{
-		sAppName = "Example";
+		arr = new float[size];
 	}
 
-	bool OnUserCreate() override
+	~Layer()
 	{
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		float mousex = GetMouseX() * 2.0f / ScreenWidth();
-		uint32_t bits = *(uint32_t*)&mousex;
-
-		// Clear Screen
-		Clear(olc::BLACK);
-
-		DrawString(0, 0, std::to_string(mousex));
-
-		// Draw 32 bits, white if bit is set, black if not
-		for (int i = 0; i < 32; i++)
-			DrawCircle((ScreenWidth() >> 1) + (i - 16) * 8, (ScreenHeight() >> 1), 4, (bits >> i) & 1 ? olc::WHITE : olc::BLACK);
-
-		return true;
+		delete[] arr;
 	}
 };
 
-float randf()
+struct SomeLayer : Layer
 {
-	return (float)rand() / (float)RAND_MAX;
-}
+	SomeLayer(int size) : Layer(size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = 0.0f;
+		}
+	}
+};
+
+struct Network
+{
+	std::vector<Layer*> layers;
+
+	~Network()
+	{
+		for (auto layer : layers)
+		{
+			delete layer;
+		}
+	}
+
+	void AddLayer(Layer* layer)
+	{
+		layers.emplace_back(layer);
+	}
+};
 
 int main()
 {
-	auto start = std::chrono::high_resolution_clock::now();
-	int sum = 0;
-	for (int i = 0; i < 10000000; ++i)
-		sum += abs(randf()) > 1;
-		//sum += 
+	Network net;
+	net.AddLayer(new SomeLayer(10));
 
-	auto finish = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<double> elapsed = finish - start;
-	printf("Elapsed time: %f\n", elapsed.count());
-
-	return 0;
-
-	Example demo;
-	if (demo.Construct(1000, 500, 1, 1))
-		demo.Start();
 	return 0;
 }
