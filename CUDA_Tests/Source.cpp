0e9cf8f27df643e4dafad70e844fd6b949720bba
theProgrammingBox <justinboxgea@gmail.com>
2023-08-13 09:43:33 -0700

    with comments

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -3,24 +3,12 @@
 #include <utility>
 #include <cuda_runtime.h>
 
-/*
-Project Description:
-This project is a CUDA GPU Memory Mapping test. The goal is to find all the
-memory fragmentation sizes that can be allocated on the GPU. This is done by
-allocating a large block of memory and then freeing it until the allocation
-fails. The size of the allocation is then recorded and not deallocated. This
-process is repeated until a memory of size 1 can no longer be allocated. The
-size of the allocations are then printed to the console.
-*/
-
-int main()
+void MapGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
 {
-	std::vector<std::pair<size_t, float*>*> fragments;
-
 	size_t freeMem, totalMem;
 	cudaMemGetInfo(&freeMem, &totalMem);
 	freeMem >>= 2;
-	printf("Free memory: %llu floats\n", freeMem);
+	//printf("Free memory: %zu floats\n", freeMem);
 
 	std::pair<size_t, float*>* frag;
 	size_t low, high, guess;
@@ -35,7 +23,7 @@ int main()
 			err = cudaMalloc((void**)&frag->second, guess << 2);
 			err == cudaSuccess ? low = guess + 1 : high = guess - 1;
 			cudaFree(frag->second);
-			printf("Low: %llu, High: %llu, Guess: %llu\n", low, high, guess);
+			//printf("Low: %zu, High: %zu, Guess: %zu\n", low, high, guess);
 		} while (low <= high);
 		low--;
 
@@ -43,26 +31,42 @@ int main()
 		{
 			frag->first = low;
 
-			err = cudaMalloc((void**)&frag->second, low << 2);
+			cudaMalloc((void**)&frag->second, low << 2);
+			/*err = cudaMalloc((void**)&frag->second, low << 2);
 			if (err != cudaSuccess)
-				printf("Failed to allocate memory of size %llu\n", low);
+				printf("Failed to allocate memory of size %zu\n", low);
 			else
-				printf("Allocated %llu floats\n", low);
+				printf("Allocated %zu floats\n", low);*/
 
 			freeMem -= low;
-			printf("Free memory: %llu floats\n", freeMem);
+			//printf("Free memory: %zu floats\n", freeMem);
 
-			fragments.push_back(frag);
+			fragments->emplace_back(frag);
 		}
 	} while (low > 0);
 	delete frag;
+}
 
-	for (auto& frag : fragments)
+void PrintGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
+{
+	for (std::pair<size_t, float*>* frag : *fragments)
 	{
-		printf("Allocated %llu floats at %p\n", frag->first, frag->second);
+		printf("Allocated %zu floats at %p\n", frag->first, frag->second);
 		cudaFree(frag->second);
 		delete frag;
 	}
+}
+
+int main()
+{
+	float* a;
+	cudaMalloc((void**)&a, 345 << 2);
+
+	std::vector<std::pair<size_t, float*>*> fragments;
+	MapGpuMem(&fragments);
+	PrintGpuMem(&fragments);
+
+	cudaFree(a);
 
 	return 0;
 }
