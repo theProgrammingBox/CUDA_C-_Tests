bcdadfad8c3301c6a9055979a25b20714ec1590a
theProgrammingBox <justinboxgea@gmail.com>
2023-08-13 10:11:24 -0700

    seems like there is always free memory just in case?

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,56 +1,84 @@
-﻿#include <iostream>
+﻿#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
-#include <utility>
 #include <cuda_runtime.h>
 
-void MapGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
+struct GpuMemoryManager
 {
-	size_t freeMem, totalMem;
-	cudaMemGetInfo(&freeMem, &totalMem);
-	freeMem >>= 2;
-
-	std::pair<size_t, float*>* frag;
-	size_t low, high, guess;
-	cudaError_t err;
-	do
+	struct MemFrag
 	{
-		frag = new std::pair<size_t, float*>(high, nullptr);
-		low = 1, high = freeMem;
+		size_t size;
+		void* address;
+	};
+
+	std::vector<MemFrag*> MemFrags;
+
+	~GpuMemoryManager()
+	{
+		for (MemFrag* frag : MemFrags)
+		{
+			cudaFree(frag->address);
+			delete frag;
+		}
+	}
+
+	void MapGpuMem()
+	{
+		size_t freeMem, totalMem;
+		cudaMemGetInfo(&freeMem, &totalMem);
+
+		MemFrag* frag;
+		size_t low, high, guess;
+		cudaError_t err;
 		do
 		{
-			guess = (low + high) >> 1;
-			err = cudaMalloc((void**)&frag->second, guess << 2);
-			err == cudaSuccess ? low = guess + 1 : high = guess - 1;
-			cudaFree(frag->second);
-		} while (low <= high);
-		low--;
+			frag = new MemFrag;
+			low = 1, high = freeMem;
+			do
+			{
+				guess = (low + high) >> 1;
+				err = cudaMalloc((void**)&frag->address, guess);
+				err == cudaSuccess ? low = guess + 1 : high = guess - 1;
+				cudaFree(frag->address);
+			} while (low <= high);
+			low--;
 
-		if (low > 0)
-		{
-			frag->first = low;
-			cudaMalloc((void**)&frag->second, low << 2);
-			freeMem -= low;
-			fragments->emplace_back(frag);
-		}
-	} while (low > 0);
-	delete frag;
-}
-
-void PrintGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
-{
-	for (std::pair<size_t, float*>* frag : *fragments)
-	{
-		printf("Allocated %zu floats at %p\n", frag->first, frag->second);
-		cudaFree(frag->second);
+			if (low > 0)
+			{
+				frag->size = low;
+				cudaMalloc((void**)&frag->address, low);
+				freeMem -= low;
+				MemFrags.emplace_back(frag);
+			}
+		} while (low > 0);
 		delete frag;
 	}
-}
+
+	void PrintGpuMem()
+	{
+		for (MemFrag* frag : MemFrags)
+			printf("Allocated %zu bytes at %p\n", frag->size, frag->address);
+	}
+};
 
 int main()
 {
-	std::vector<std::pair<size_t, float*>*> fragments;
-	MapGpuMem(&fragments);
-	PrintGpuMem(&fragments);
+	GpuMemoryManager manager;
+
+	manager.MapGpuMem();
+	manager.PrintGpuMem();
+
+	size_t freeMem, totalMem;
+	cudaMemGetInfo(&freeMem, &totalMem);
+	printf("Free memory: %zu\n", freeMem);
+
+	GpuMemoryManager manager2;
+
+	manager2.MapGpuMem();
+	manager2.PrintGpuMem();
+
+	cudaMemGetInfo(&freeMem, &totalMem);
+	printf("Free memory: %zu\n", freeMem);
 
 	return 0;
 }
