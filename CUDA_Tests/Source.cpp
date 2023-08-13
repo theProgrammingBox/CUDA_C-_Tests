8d5b234c66a1de66155e02ac7676f71c5fa2980b
theProgrammingBox <justinboxgea@gmail.com>
2023-08-13 09:44:13 -0700

    without comments

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -8,7 +8,6 @@ void MapGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
 	size_t freeMem, totalMem;
 	cudaMemGetInfo(&freeMem, &totalMem);
 	freeMem >>= 2;
-	//printf("Free memory: %zu floats\n", freeMem);
 
 	std::pair<size_t, float*>* frag;
 	size_t low, high, guess;
@@ -23,24 +22,14 @@ void MapGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
 			err = cudaMalloc((void**)&frag->second, guess << 2);
 			err == cudaSuccess ? low = guess + 1 : high = guess - 1;
 			cudaFree(frag->second);
-			//printf("Low: %zu, High: %zu, Guess: %zu\n", low, high, guess);
 		} while (low <= high);
 		low--;
 
 		if (low > 0)
 		{
 			frag->first = low;
-
 			cudaMalloc((void**)&frag->second, low << 2);
-			/*err = cudaMalloc((void**)&frag->second, low << 2);
-			if (err != cudaSuccess)
-				printf("Failed to allocate memory of size %zu\n", low);
-			else
-				printf("Allocated %zu floats\n", low);*/
-
 			freeMem -= low;
-			//printf("Free memory: %zu floats\n", freeMem);
-
 			fragments->emplace_back(frag);
 		}
 	} while (low > 0);
@@ -59,14 +48,9 @@ void PrintGpuMem(std::vector<std::pair<size_t, float*>*>* fragments)
 
 int main()
 {
-	float* a;
-	cudaMalloc((void**)&a, 345 << 2);
-
 	std::vector<std::pair<size_t, float*>*> fragments;
 	MapGpuMem(&fragments);
 	PrintGpuMem(&fragments);
-
-	cudaFree(a);
 
 	return 0;
 }
