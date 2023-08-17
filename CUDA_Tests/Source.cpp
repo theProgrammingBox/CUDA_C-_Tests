d6c4c3eb5e6377757bb64f92600884d359d25e16
theProgrammingBox <justinboxgea@gmail.com>
2023-08-17 12:13:31 -0700

    it seems that doing culbas math doesn't mess with the memory mapper. could confirm again

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -114,7 +114,7 @@ int main()
 	const size_t CSize = m * k;
 
 	d_A = manager.MemFrags[0]->address;
-	d_B = manager.MemFrags[0]->address + ASize - 1;
+	d_B = manager.MemFrags[0]->address + ASize;
 	d_C = manager.MemFrags[0]->address + ASize + BSize;
 
 	CurandGenerateUniformf32(curandGenerator, d_A, ASize);
@@ -129,6 +129,46 @@ int main()
 
 	PrintTensorf32(n, m, h_A);
 	PrintTensorf32(k, n, h_B);
+
+	const float alpha = 1.0f;
+	const float beta = 0.0f;
+
+	cublasStatus = cublasSgemm(
+		cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
+		k, m, n,
+		&alpha,
+		d_B, k,
+		d_A, n,
+		&beta,
+		d_C, k);
+
+	if (cublasStatus != CUBLAS_STATUS_SUCCESS) {
+		printf("cublasSgemm failed with error code %d\n", cublasStatus);
+		return EXIT_FAILURE;
+	}
+
+	cudaMemcpy(h_C, d_C, CSize * sizeof(float), cudaMemcpyDeviceToHost);
+
+	PrintTensorf32(k, m, h_C);
+
+	cublasStatus = cublasSgemmStridedBatched(
+		cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
+		k, m, n,
+		&alpha,
+		d_B, k, BSize,
+		d_A, n, ASize,
+		&beta,
+		d_C, k, CSize,
+		1);
+
+	if (cublasStatus != CUBLAS_STATUS_SUCCESS) {
+		printf("cublasSgemmStridedBatched failed with error code %d\n", cublasStatus);
+		return EXIT_FAILURE;
+	}
+
+	cudaMemcpy(h_C, d_C, CSize * sizeof(float), cudaMemcpyDeviceToHost);
+
+	PrintTensorf32(k, m, h_C);
 
 	cudaMemGetInfo(&freeMem, &totalMem);
 	printf("Free memory: %zu\n", freeMem);
