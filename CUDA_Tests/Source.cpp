74acaa19fdf6feb2d594ffb8858e6795ec83c74a
theProgrammingBox <justinboxgea@gmail.com>
2023-08-20 16:39:11 -0700

    optimized

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -1,56 +1,24 @@
 ﻿#define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-uint32_t XXH_readLE32(const void* memPtr)
+olc::Pixel hash(uint32_t idx, uint32_t seed, uint32_t offset)
 {
-	const uint8_t* bytePtr = (const uint8_t*)memPtr;
-	return bytePtr[0]
-		| ((uint32_t)bytePtr[1] << 8)
-		| ((uint32_t)bytePtr[2] << 16)
-		| ((uint32_t)bytePtr[3] << 24);
-}
+	seed ^= idx;
+	seed *= 0xBAC57D37;
+	seed ^= seed >> 15;
+	seed ^= offset;
+	seed *= 0x24F66AC9;
+	seed ^= seed >> 17;
 
-uint64_t XXH_readLE64(const void* memPtr)
-{
-	const uint8_t* bytePtr = (const uint8_t*)memPtr;
-	return bytePtr[0]
-		| ((uint64_t)bytePtr[1] << 8)
-		| ((uint64_t)bytePtr[2] << 16)
-		| ((uint64_t)bytePtr[3] << 24)
-		| ((uint64_t)bytePtr[4] << 32)
-		| ((uint64_t)bytePtr[5] << 40)
-		| ((uint64_t)bytePtr[6] << 48)
-		| ((uint64_t)bytePtr[7] << 56);
-}
-
-static uint64_t XXH3_rrmxmx(uint64_t h64, uint64_t len)
-{
-	h64 ^= _rotl64(h64, 49) ^ _rotl64(h64, 24);
-	h64 *= 0x9FB21C651E98DF25ULL;
-	h64 ^= (h64 >> 35) + 8;
-	h64 *= 0x9FB21C651E98DF25ULL;
-	return h64 ^ (h64 >> 28);
-}
-
-olc::Pixel hash(const uint8_t* idx, uint64_t seed, const uint8_t* offset)
-{
-	seed ^= (uint64_t)_byteswap_ulong((uint32_t)seed) << 32;
-	uint32_t const input1 = XXH_readLE32(idx);
-	uint32_t const input2 = XXH_readLE32(idx + 4);
-	uint64_t const bitflip = (0x0E4125884092CA03ULL ^ XXH_readLE64(offset)) - seed;
-	uint64_t const input64 = input2 + (((uint64_t)input1) << 32);
-	uint64_t const keyed = input64 ^ bitflip;
-	uint64_t output = XXH3_rrmxmx(keyed, 8);
-
-	float color = (float)keyed / (float)UINT64_MAX * 255.0f;
+	float const color = seed * 0.00000000023283064365386962890625f;
 	return olc::PixelF(color, color, color);
 }
 
 class Example : public olc::PixelGameEngine
 {
 public:
-	uint64_t seed;
-	uint64_t offset;
+	uint32_t seed;
+	uint32_t offset;
 
 	void render()
 	{
@@ -58,7 +26,7 @@ public:
 			for (int y = 0; y < ScreenHeight(); y++)
 			{
 				uint32_t idx = y * ScreenWidth() + x;
-				Draw(x, y, hash((uint8_t*)&idx, seed, (uint8_t*)&offset));
+				Draw(x, y, hash(idx, seed, offset));
 			}
 	}
 
@@ -74,13 +42,13 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		/*if (GetKey(olc::Key::SPACE).bPressed)
-		{*/
+		if (GetKey(olc::Key::SPACE).bPressed)
+		{
 			seed++;
 			offset += 3;
 
 			render();
-		//}
+		}
 		return true;
 	}
 };
@@ -88,7 +56,7 @@ public:
 int main()
 {
 	Example demo;
-	if (demo.Construct(1920, 1080, 1, 1))
+	if (demo.Construct(1440, 810, 1, 1))
 		demo.Start();
 	return 0;
 }
