3f3c85ccaac8f78d509f554a10b819628be67c4a
theProgrammingBox <justinboxgea@gmail.com>
2023-09-19 17:15:11 -0700

    backoprop works, working on adam

--- CUDA_Tests/Source.cpp
+++ CUDA_Tests/Source.cpp
@@ -8,7 +8,8 @@ struct Layer {
 	float* learningRate;
 
 	size_t inputWidth;
-	float* deviceInputTensor;
+	float* deviceForwardInputTensor;
+	float* deviceBackwardOutputTensor;
 
 	Layer(cublasHandle_t* cublasHandle, GpuMemoryManager* gpuMemoryManager, GpuRand* gpuRand, size_t* inputHeight, float* learningRate) :
 		cublasHandle(cublasHandle), gpuMemoryManager(gpuMemoryManager), gpuRand(gpuRand), inputHeight(inputHeight), learningRate(learningRate) {}
@@ -16,55 +17,93 @@ struct Layer {
 	void AssignInputDim(size_t inputWidth) {
 		this->inputWidth = inputWidth;
 	}
-	void AssignInputTensor(float* deviceInputTensor) {
-		this->deviceInputTensor = deviceInputTensor;
-	}
 	virtual void DescribeTensorDetails() = 0;
+	void ConnectTensors(float* deviceForwardInputTensor, float* deviceBackwardOutputTensor) {
+		this->deviceForwardInputTensor = deviceForwardInputTensor;
+		this->deviceBackwardOutputTensor = deviceBackwardOutputTensor;
+	}
 	virtual void InitializeParameters() = 0;
 	virtual void Forward() = 0;
 	virtual void Backward() = 0;
-	virtual void PrintParameters() = 0;
+	virtual void PrintForward() = 0;
+	virtual void PrintBackward() = 0;
 };
 
 struct WeightLayer : Layer {
 	size_t outputWidth;
-	float* deviceWeightTensor;
-	float* deviceOutputTensor;
+	float* deviceForwardWeightTensor;
+	float* deviceForwardOutputTensor;
+	float* deviceBackwardWeightTensor;
+	float* deviceBackwardInputTensor;
 
 	WeightLayer(cublasHandle_t* cublasHandle, GpuMemoryManager* gpuMemoryManager, GpuRand* gpuRand, size_t* inputHeight, float* learningRate, size_t outputWidth) :
 		Layer(cublasHandle, gpuMemoryManager, gpuRand, inputHeight, learningRate), outputWidth(outputWidth) {}
 
 	void DescribeTensorDetails() {
-		gpuMemoryManager->ManageStatic(&deviceWeightTensor, inputWidth * outputWidth);
-		gpuMemoryManager->ManageDynamic(&deviceOutputTensor, outputWidth);
+		gpuMemoryManager->ManageStatic(&deviceForwardWeightTensor, inputWidth * outputWidth);
+		gpuMemoryManager->ManageDynamic(&deviceForwardOutputTensor, outputWidth);
+		gpuMemoryManager->ManageStatic(&deviceBackwardWeightTensor, inputWidth * outputWidth);
+		gpuMemoryManager->ManageDynamic(&deviceBackwardInputTensor, inputWidth);
 	}
 
 	void InitializeParameters() {
-		gpuRand->Rand(deviceWeightTensor, inputWidth * outputWidth);
+		gpuRand->Rand(deviceForwardWeightTensor, inputWidth * outputWidth);
 	}
 
 	void Forward() {
 		float alpha = 1.0f;
 		float beta = 0.0f;
+
 		FailIf(
 			cublasSgemm(
 				*cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
 				outputWidth, *inputHeight, inputWidth,
 				&alpha,
-				deviceWeightTensor, outputWidth,
-				deviceInputTensor, inputWidth,
+				deviceForwardWeightTensor, outputWidth,
+				deviceForwardInputTensor, inputWidth,
 				&beta,
-				deviceOutputTensor, outputWidth
+				deviceForwardOutputTensor, outputWidth
 			) != CUBLAS_STATUS_SUCCESS, "cublasSgemm failed"
 		);
 	}
 
 	void Backward() {
+		float alpha = 1.0f;
+		float beta = 0.0f;
+
+		FailIf(
+			cublasSgemm(
+				*cublasHandle, CUBLAS_OP_N, CUBLAS_OP_T,
+				outputWidth, inputWidth, *inputHeight,
+				&alpha,
+				deviceBackwardOutputTensor, outputWidth,
+				deviceForwardInputTensor, inputWidth,
+				&beta,
+				deviceBackwardWeightTensor, outputWidth
+			) != CUBLAS_STATUS_SUCCESS, "cublasSgemm failed"
+		);
+
+		FailIf(
+			cublasSgemm(
+				*cublasHandle, CUBLAS_OP_T, CUBLAS_OP_N,
+				inputWidth, *inputHeight, outputWidth,
+				&alpha,
+				deviceForwardWeightTensor, outputWidth,
+				deviceBackwardOutputTensor, outputWidth,
+				&beta,
+				deviceBackwardInputTensor, inputWidth
+			) != CUBLAS_STATUS_SUCCESS, "cublasSgemm failed"
+		);
 	}
 
-	void PrintParameters() {
-		PrintDeviceTensorf32(inputWidth, outputWidth, deviceWeightTensor, "deviceWeightTensor");
-		PrintDeviceTensorf32(*inputHeight, outputWidth, deviceOutputTensor, "deviceOutputTensor");
+	void PrintForward() {
+		PrintDeviceTensorf32(inputWidth, outputWidth, deviceForwardWeightTensor, "deviceForwardWeightTensor");
+		PrintDeviceTensorf32(*inputHeight, outputWidth, deviceForwardOutputTensor, "deviceForwardOutputTensor");
+	}
+
+	void PrintBackward() {
+		PrintDeviceTensorf32(inputWidth, outputWidth, deviceBackwardWeightTensor, "deviceBackwardWeightTensor");
+		PrintDeviceTensorf32(*inputHeight, inputWidth, deviceBackwardInputTensor, "deviceBackwardInputTensor");
 	}
 };
 
@@ -84,8 +123,10 @@ int main() {
 	size_t inputWidth = 3;
 	size_t outputWidth = 2;
 
-	float* deviceInputTensor;
-	gpuMemoryManager.ManageDynamic(&deviceInputTensor, inputWidth);
+	float* deviceForwardInputTensor;
+	float* deviceBackwardOutputTensor;
+	gpuMemoryManager.ManageDynamic(&deviceForwardInputTensor, inputWidth);
+	gpuMemoryManager.ManageDynamic(&deviceBackwardOutputTensor, outputWidth);
 
 	Layer* weightLayer = new WeightLayer(&cublasHandle, &gpuMemoryManager, &gpuRand, &inputHeight, &learningRate, outputWidth);
 	weightLayer->AssignInputDim(inputWidth);
@@ -94,16 +135,26 @@ int main() {
 
 	size_t maxInputHeight;
 	gpuMemoryManager.Allocate(maxInputHeight);
-	weightLayer->AssignInputTensor(deviceInputTensor);
+	weightLayer->ConnectTensors(deviceForwardInputTensor, deviceBackwardOutputTensor);
 	weightLayer->InitializeParameters();
 
 
 	FailIf(inputHeight > maxInputHeight, "inputHeight > maxInputHeight");
-	gpuRand.Rand(deviceInputTensor, inputHeight * inputWidth);
+	gpuRand.Rand(deviceForwardInputTensor, inputHeight * inputWidth);
+	gpuRand.Rand(deviceBackwardOutputTensor, outputWidth * inputHeight);
+
+
 	weightLayer->Forward();
-	//weightLayer->Backward();
-	PrintDeviceTensorf32(inputHeight, inputWidth, deviceInputTensor, "deviceInputTensor");
-	weightLayer->PrintParameters();
+	weightLayer->Backward();
+
+
+	PrintDeviceTensorf32(inputHeight, inputWidth, deviceForwardInputTensor, "deviceForwardInputTensor");
+	weightLayer->PrintForward();
+
+	PrintDeviceTensorf32(inputHeight, outputWidth, deviceBackwardOutputTensor, "deviceBackwardOutputTensor");
+	weightLayer->PrintBackward();
+
+
 	printf("Press any key to exit\n");
 
 
